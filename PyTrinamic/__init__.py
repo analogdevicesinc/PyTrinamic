70a70f776e63a21005816f3d501f4d522950eaa1
ED <dressler@trinamic.com>
2018-12-30 21:14:54 +0100

    Initial commit.

---
+++ PyTrinamic/__init__.py
@@ -0,0 +1,7 @@
+'''
+Created on 30.12.2018
+
+@author: ED
+'''
+
+name = "PyTrinamic"
---
+++ setup.py
@@ -0,0 +1,26 @@
+'''
+Created on 30.12.2018
+
+@author: ED
+'''
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="PyTrinamic",
+    version="0.0.1",
+    author="ED,..",
+    author_email="tmc_info@trinamic.com",
+    description="TRINAMIC's Python Technology Access Package.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/trinamic/PyTrinamic",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: GNU GENERAL PUBLIC LICENSE Version 3",
+        "Operating System :: OS Independent",
+    ],
+)
