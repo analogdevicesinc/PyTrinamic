10ee0675f698831e4ee3f3b39194654e39ebece5
ED <dressler@trinamic.com>
2019-01-02 20:18:18 +0100

    Added new files to setup.py.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="PyTrinamic",
-    version="0.0.7",
+    version="0.0.8",
     author="ED,..",
     author_email="tmc_info@trinamic.com",
     description="TRINAMIC's Python Technology Access Package.",
@@ -19,9 +19,14 @@ setuptools.setup(
     url="https://github.com/trinamic/PyTrinamic",
     packages=setuptools.find_packages(),
     py_modules=["PyTrinamic/connections/SerialInterface",
-                "PyTrinamic/modules/TMCM_0010_OPC",
-                "PyTrinamic/examples/TMCM-0010-OPC_config_check",
-                "PyTrinamic/examples/TMCM-0010-OPC_config_update"],
+                "PyTrinamic/evalboards/TMC4671_Eval",
+                "PyTrinamic/examples/evalboards/TMC4671_Eval_BLDC_ABN_encoder",
+                "PyTrinamic/examples/evalboards/TMC4671_Eval_BLDC_open_loop",
+                "PyTrinamic/examples/modules/TMCM-0010-OPC_config_check",
+                "PyTrinamic/examples/modules/TMCM-0010-OPC_config_update",
+                "PyTrinamic/ICs/TMC4671/TMC4671_Mask_Shift",
+                "PyTrinamic/ICs/TMC4671/TMC4671_Register",
+                "PyTrinamic/modules/TMCM_0010_OPC"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
