3b27e9ae3915d8d10fcabd603a9d08cf2fae4928
ed <dressler@trinamic.com>
2019-01-03 17:48:27 +0100

    Added TMC4671 direct UART interface.

--- PyTrinamic/__init__.py
+++ PyTrinamic/__init__.py
@@ -20,3 +20,10 @@ def showAvailableComPorts():
        
     print("Available COM ports: " + str(connected))
     return 0;
+
+def firstAvailableComPort():
+    comlist = serial.tools.list_ports.comports()
+    connected = []
+    for element in comlist:
+        connected.append(element.device)
+        return element.device;
---
+++ PyTrinamic/connections/ic_uart_interface.py
@@ -0,0 +1,79 @@
+'''
+Created on 30.12.2018
+
+@author: ED
+'''
+
+import struct
+from serial import Serial
+#from PyTrinamic.TMCL import TMCL, TMCL_Command, TMCL_Request, TMCL_Reply
+
+REGISTER_PACKAGE_STRUCTURE   = ">BI"
+REGISTER_PACKAGE_LENGTH      = 5
+
+class Register_Request(object):
+    def __init__(self, address, value):
+        self.address = address
+        self.value = value & 0xFFFFFFFF
+
+    def toBuffer(self):
+        return struct.pack(REGISTER_PACKAGE_STRUCTURE, self.address, self.value)
+   
+    def dump(self): 
+        print("Register_Request: " + str(self.address) + "," + str(self.value))
+
+class Register_Reply(object):
+    def __init__(self, reply_struct):
+        self.address = reply_struct[0]
+        self.value = reply_struct[1]
+
+    def dump(self): 
+        print("Register_Reply:   " + str(self.address) + "," + str(self.value))
+
+    def value(self):
+        return self.value
+
+    def valueLower16Bit(self):
+        return (self.value) & 0xFFFF
+    
+    def valueUpper16Bit(self):
+        return (self.value>>16) & 0xFFFF
+    
+class ic_uart_interface(object):
+
+    def __init__(self, comPort):
+        self.debugEnabled = False
+        self.serial = Serial(comPort, 9600)#115200)
+        print("Open port: " + self.serial.portstr)
+        
+    def close( self ):
+        print("Close port: " + self.serial.portstr)
+        self.serial.close()
+        return 0;
+    
+    def enableDebug(self, enable):
+        self.debugEnabled = enable
+
+    def send ( self, address, value ):
+       
+        "prepare TMCL request"
+        request = Register_Request(address, value)
+        
+        if self.debugEnabled:
+            request.dump()
+        
+        "send request, wait, and handle reply"
+        self.serial.write(request.toBuffer())
+        reply = Register_Reply(struct.unpack(REGISTER_PACKAGE_STRUCTURE, self.serial.read(REGISTER_PACKAGE_LENGTH)))
+        
+        if self.debugEnabled:
+            reply.dump()
+
+        return reply
+
+    " motion controller register access "
+    def writeMC(self, registerAddress, value):
+        return self.send(registerAddress | 0x80, value)
+    
+    def readMC(self, registerAddress):
+        return self.send(registerAddress, 0)
