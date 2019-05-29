79bd4dbce94950bd3ccf9b50d3c6b7bd87c8923e
Lenard Hess <Hess@trinamic.com>
2019-05-29 13:00:50 +0200

    Updated serial TMCL interface

--- PyTrinamic/connections/serial_tmcl_interface.py
+++ PyTrinamic/connections/serial_tmcl_interface.py
@@ -6,17 +6,18 @@ Created on 30.12.2018
 
 from serial import Serial
 from serial import SerialException
+import PyTrinamic
 from PyTrinamic.connections.tmcl_interface import tmcl_interface
 
 class serial_tmcl_interface(tmcl_interface):
 
-    def __init__(self, comPort, baudrate=115200, hostID=2, moduleID=1, debug=False):
+    def __init__(self, comPort, datarate=115200, hostID=2, moduleID=1, debug=False):
         if type(comPort) != str:
             raise TypeError;
 
         tmcl_interface.__init__(self, hostID, moduleID, debug)
 
-        self.__baudrate = baudrate
+        self.__baudrate = datarate
 
         try:
             self.__serial = Serial(comPort, self.__baudrate)
@@ -26,20 +27,6 @@ class serial_tmcl_interface(tmcl_interface):
         if self._debug:
             print("Open port: " + self.__serial.portstr)
 
-    # Overridden functions for the tmcl_interface
-    def _send(self, hostID, moduleID, data):
-        del hostID, moduleID
-
-        self.__serial.write(data)
-
-    def _recv(self, hostID, moduleID):
-        del hostID, moduleID
-
-        return self.__serial.read(9)
-
-    def printInfo(self):
-        print("Connection: type=serial_tmcl_interface com=" + self.__serial.portstr + " baud=" + str(self.baudrate))
-
     def close(self):
         if self._debug:
             print("Close port: " + self.__serial.portstr)
@@ -47,5 +34,40 @@ class serial_tmcl_interface(tmcl_interface):
         self.__serial.close()
         return 0;
 
+    def _send(self, hostID, moduleID, data):
+        """
+            Send the bytearray parameter [data].
+
+            This is a required override function for using the tmcl_interface
+            class.
+        """
+        del hostID, moduleID
+
+        self.__serial.write(data)
+
+    def _recv(self, hostID, moduleID):
+        """
+            Read 9 bytes and return them as a bytearray.
+
+            This is a required override function for using the tmcl_interface
+            class.
+        """
+        del hostID, moduleID
+
+        return self.__serial.read(9)
+
+    def printInfo(self):
+        print("Connection: type=serial_tmcl_interface com=" + self.__serial.portstr + " baud=" + str(self.baudrate))
+
     def enableDebug(self, enable):
         self._debug = enable
+
+    @staticmethod
+    def list():
+        """
+            Return a list of available connection ports as a list of strings.
+
+            This function is required for using this interface with the
+            connection manager.
+        """
+        return PyTrinamic.getAvailableSerialPorts()
