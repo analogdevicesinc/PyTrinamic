6020d83703a708f4c5fb9a68b6cd5c1a8d6bb3f0
Lenard Hess <Hess@trinamic.com>
2019-05-31 11:40:07 +0200

    Cleaned up tmcl_interface.py
    - Updated docstrings
    - Renamed the "axis" parameter of the globalParameter() functions to
      "bank".
    - Changed strings used as comments to actual comments
    - renamed _hostID and _moduleID to _HOST_ID and _MODULE_ID respectively,
      as theyre supposed to be constant.

--- PyTrinamic/connections/tmcl_interface.py
+++ PyTrinamic/connections/tmcl_interface.py
@@ -13,89 +13,102 @@ class tmcl_interface():
     This class is a base class for sending TMCL commands over a communication
     interface.
 
-    Constructor parameters:
-        host_id:
-            Type: int, optional, default value: 2
-            The ID of the TMCL host. This id is the same for each module
-            when communicating with multiple modules.
-        module_id:
-            Type: int, optional, default value: 1
-            The default module ID to use when no ID is given to any of
-            the tmcl_interface functions.
-        debug:
-            Type: bool, optional, default: False
-            A switch for enabling debug mode. Can be changed with enableDebug().
-            In debug mode results all sent and received TMCL packets get dumped
-            to stdout. The boolean _debug attribute holds the current state of
-            debug mode - subclasses may read it to print futher debug output.
+    Each instance of this class represents one TMCL host. The bus connection for
+    the TMCL communication is represented by a subclass inheriting this base
+    class. An application with multiple busses should therefore use subclasses
+    for all types of busses (e.g. one USB TMCL interface and one serial TMCL
+    interface) and create exactly one instance of one of those subclasses per
+    bus.
 
-    A subclass is required over override the following functions:
+    A subclass is required to override the following functions:
+        _send(self, hostID, moduleID, data)
+        _recv(self, hostID, moduleID)
 
-    _send(self, hostID, moduleID, data):
-        Send the bytearray [data]. The length of [data] is 9. The hostID and
-        moduleID parameters may be used for extended addressing options
-        available on the implemented communication interface.
-    _recv(self, hostID, moduleID):
-        Receive a reply and return it as a bytearray. The length of the returned
-        byte array is 9. The hostID and moduleID parameters may be used for
-        extended addressing options available on the implemented communication
-        interface.
+    A subclass may use the boolean _debug attribute to toggle printing further
+    debug output.
+
+    A subclass may read the _HOST_ID and _MODULE_ID parameters.
     """
 
     def __init__(self, hostID=2, moduleID=1, debug=False):
+        """
+        Parameters:
+            hostID:
+                Type: int, optional, default value: 2
+                The ID of the TMCL host. This ID is the same for each module
+                when communicating with multiple modules.
+            moduleID:
+                Type: int, optional, default value: 1
+                The default module ID to use when no ID is given to any of the
+                tmcl_interface functions. When only communicating with one
+                module a script can omit the moduleID for all TMCL interface
+                calls by declaring this default value once at the start.
+            debug:
+                Type: bool, optional, default: False
+                A switch for enabling debug mode. Can be changed with
+                enableDebug(). In debug mode all sent and received TMCL packets
+                get dumped to stdout. The boolean _debug attribute holds the
+                current state of debug mode - subclasses may read it to print
+                futher debug output.
+        """
         if not(type(hostID) == type(moduleID) == int):
             raise TypeError
 
         if not type(debug) == bool:
             raise TypeError
 
-        self._hostID    = hostID   & 0xFF
-        self._moduleID  = moduleID & 0xFF
-        self._debug     = debug
+        self._HOST_ID    = hostID   & 0xFF
+        self._MODULE_ID  = moduleID & 0xFF
+        self._debug      = debug
 
     def _send(self, hostID, moduleID, data):
         """
-        Send a bytearray representing a TMCL command
+        Send the bytearray [data] representing a TMCL command. The length of
+        [data] is 9. The hostID and moduleID parameters may be used for extended
+        addressing options available on the implemented communication interface.
         """
         raise NotImplementedError("The TMCL interface requires an implementation of the _send() function")
 
     def _recv(self, hostID, moduleID):
         """
-        Receive a TMCL reply and return it as a bytearray
+        Receive a TMCL reply and return it as a bytearray. The length of the
+        returned byte array is 9. The hostID and moduleID parameters may be used
+        for extended addressing options available on the implemented
+        communication interface.
         """
         raise NotImplementedError("The TMCL interface requires an implementation of the _recv() function")
 
     def enableDebug(self, enable):
         """
-        Enable the debug mode, which dumps all TMCL datagrams written and read
+        Set the debug mode, which dumps all TMCL datagrams written and read.
         """
         if type(enable) != bool:
             raise TypeError("Expected boolean value")
 
         self._debug = enable
 
-    def send(self, opcode, op_type, motor, value, moduleID=None):
+    def send(self, opcode, opType, motor, value, moduleID=None):
         """
         Send a TMCL datagram and read back a reply. This function blocks until
         the reply has been received
         """
-        if not(type(opcode) == type(op_type) == type(motor) == type(value) == int):
+        if not(type(opcode) == type(opType) == type(motor) == type(value) == int):
             raise TypeError("Expected integer values")
 
         # If no module ID is given, use the default one
         if not moduleID:
-            moduleID = self._moduleID
+            moduleID = self._MODULE_ID
 
-        request = TMCL_Request(moduleID, opcode, op_type, motor, value)
+        request = TMCL_Request(moduleID, opcode, opType, motor, value)
 
         if self._debug:
             request.dump()
 
         # Send the request
-        self._send(self._hostID, moduleID, request.toBuffer())
+        self._send(self._HOST_ID, moduleID, request.toBuffer())
 
         # Read out the reply
-        reply = TMCL_Reply(self._recv(self._hostID, moduleID))
+        reply = TMCL_Reply(self._recv(self._HOST_ID, moduleID))
 
         if self._debug:
             reply.dump()
@@ -104,12 +117,12 @@ class tmcl_interface():
 
     def sendBoot(self, moduleID=None):
         """
-        Send the command for entering bootloader mode. This command does not
-        send a reply.
+        Send the command for entering bootloader mode. This TMCL command does
+        result in a reply.
         """
         # If no module ID is given, use the default one
         if not moduleID:
-            moduleID = self._moduleID
+            moduleID = self._MODULE_ID
 
         request = TMCL_Request(moduleID, TMCL_Command.BOOT, 0x81, 0x92, 0xA3B4C5D6)
 
@@ -117,7 +130,7 @@ class tmcl_interface():
             request.dump()
 
         # Send the request
-        self._send(self._hostID, moduleID, request.toBuffer())
+        self._send(self._HOST_ID, moduleID, request.toBuffer())
 
     def getVersionString(self, moduleID=None):
         """
@@ -127,7 +140,7 @@ class tmcl_interface():
 
         return reply.versionString()
 
-    " axis parameter access "
+    # Axis parameter access functions
     def axisParameter(self, commandType, axis, moduleID=None):
         return TMC_helpers.toSigned32(self.send(TMCL_Command.GAP, commandType, axis, 0, moduleID).value)
 
@@ -141,21 +154,21 @@ class tmcl_interface():
         self.send(TMCL_Command.SAP, commandType, axis, value, moduleID)
         self.send(TMCL_Command.STAP, commandType, axis, 0, moduleID)
 
-    " global parameter access "
-    def globalParameter(self, commandType, axis, moduleID=None):
-        return TMC_helpers.toSigned32(self.send(TMCL_Command.GGP, commandType, axis, 0, moduleID).value)
+    # Global parameter access functions
+    def globalParameter(self, commandType, bank, moduleID=None):
+        return TMC_helpers.toSigned32(self.send(TMCL_Command.GGP, commandType, bank, 0, moduleID).value)
 
-    def setGlobalParameter(self, commandType, axis, value, moduleID=None):
-        return self.send(TMCL_Command.SGP, commandType, axis, value, moduleID)
+    def setGlobalParameter(self, commandType, bank, value, moduleID=None):
+        return self.send(TMCL_Command.SGP, commandType, bank, value, moduleID)
 
-    def storeGlobalParameter(self, commandType, axis, moduleID=None):
-        return self.send(TMCL_Command.STGP, commandType, axis, 0, moduleID)
+    def storeGlobalParameter(self, commandType, bank, moduleID=None):
+        return self.send(TMCL_Command.STGP, commandType, bank, 0, moduleID)
 
-    def setAndStoreGlobalParameter(self, commandType, axis, value, moduleID=None):
-        self.send(TMCL_Command.SGP, commandType, axis, value, moduleID)
-        self.send(TMCL_Command.STGP, commandType, axis, 0, moduleID)
+    def setAndStoreGlobalParameter(self, commandType, bank, value, moduleID=None):
+        self.send(TMCL_Command.SGP, commandType, bank, value, moduleID)
+        self.send(TMCL_Command.STGP, commandType, bank, 0, moduleID)
 
-    " register access "
+    # Register access functions
     def writeMC(self, registerAddress, value, moduleID=None):
         return self.send(TMCL_Command.WRITE_MC, registerAddress, 0, value, moduleID)
 
@@ -178,7 +191,7 @@ class tmcl_interface():
     def move(self, moveType, motor, position, moduleID=None):
         return self.send(TMCL_Command.MVP, moveType, motor, position, moduleID)
 
-    " input / outputs "
+    # IO pin functions
     def analogInput(self, x, moduleID=None):
         return self.send(TMCL_Command.GIO, x, 1, 0, moduleID).value
 
