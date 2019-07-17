9f2d8c36356eea325d0ed278adef0be497b046e6
Leonard Kugis <leonard@kug.is>
2019-07-17 15:14:24 +0200

    test_TMC5130_TMCL
    Applied naming conventions for private attributes.
    Made register stability test repititions configurable.

--- PyTrinamic/tests/TMC5130_TMCL.py
+++ PyTrinamic/tests/TMC5130_TMCL.py
@@ -4,6 +4,7 @@ from openhtf.plugs import user_input
 from openhtf.util import conf
 
 import logging
+import argparse
 
 import PyTrinamic
 from PyTrinamic.connections.serial_tmcl_interface import serial_tmcl_interface
@@ -14,36 +15,43 @@ from PyTrinamic.cli import select_com_port_by_name
 class test_TMC5130_TMCL(object):
     __VERSION = "1.0.0"
     def __init__(self, cm):
-        self.test = htf.Test(self.connectivity.with_args(_self=self), self.registers.with_args(_self=self),
+        parser = argparse.ArgumentParser(description='ConnectionManager to setup connections dynamically and interactively')
+        parser.add_argument('--register-tests', dest='register_tests', action='store', nargs=1, type=int, default=[100],
+                            help='Number of register stability tests to run (default: %(default)s)')
+
+        args = parser.parse_known_args()[0]
+
+        self.__test = htf.Test(self.connectivity.with_args(_self=self), self.registers.with_args(_self=self),
             test_name="TMC5130 IC test", test_description="TMC5130 IC general test", test_version=self.__VERSION)
-        self.test.add_output_callbacks(json_factory.OutputToJSON('./{dut_id}.%(name)s.json' % {"name": self.__class__.__name__}, indent=2))
-        self.interface = None
-        self.cm = cm
-        self.tmc5130 = None
-        self.register_repititions = 0
+        self.__test.add_output_callbacks(json_factory.OutputToJSON('./{dut_id}.%(name)s.json' % {"name": self.__class__.__name__}, indent=2))
+        self.__interface = None
+        self.__cm = cm
+        self.__register_tests = args.register_tests[0]
+        self.__tmc5130 = None
+        self.__register_repititions = 0
     def execute(self):
-        self.test.execute(test_start=user_input.prompt_for_test_start())
+        self.__test.execute(test_start=user_input.prompt_for_test_start())
     @htf.measures(htf.Measurement('connectivity_measurement'))
     def connectivity(test, _self):
         test.logger.info("Instantiating interface")
-        _self.interface = _self.cm.connect()
+        _self.__interface = _self.__cm.connect()
         test.logger.info("Instantiating chip")
-        _self.tmc5130 = TMC5130_eval(_self.interface)
+        _self.__tmc5130 = TMC5130_eval(_self.__interface)
         test.measurements.connectivity_measurement = 'Connectivity measurement stub'
     @htf.measures(htf.Measurement('pin_state_default_measurement'))
     def pin_state_default(test, _self):
         pin_states = []
-        _self.interface.globalParameter()
+        _self.__interface.globalParameter()
     @htf.measures(htf.Measurement('registers_measurement'))
     def registers(test, _self):
         values = {}
-        test.logger.info(f"Register stability test #{_self.register_repititions}")
-        for register in _self.tmc5130.registers.list:
-            values[register] = _self.tmc5130.readRegister(register)
+        test.logger.info(f"Register stability test #{_self.__register_repititions}")
+        for register in _self.__tmc5130.registers.list:
+            values[register] = _self.__tmc5130.readRegister(register)
             test.logger.debug("0x{:02X}: {}".format(register, values[register]))
         test.measurements.registers_measurement = values
-        _self.register_repititions = _self.register_repititions + 1
-        if(_self.register_repititions < 1000):
+        _self.__register_repititions = _self.__register_repititions + 1
+        if(_self.__register_repititions < _self.__register_tests):
             return htf.PhaseResult.REPEAT
 
 if __name__ == '__main__':
