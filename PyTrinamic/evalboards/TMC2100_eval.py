71c9fc7a3956c61df73d0d4f2bfe7ce6eccc41a6
JM <j.marsollek@web.de>
2019-11-05 15:52:21 +0100

    Changed all drivers to use read/writeDRV instead of read/writeMC

--- PyTrinamic/evalboards/TMC2100_eval.py
+++ PyTrinamic/evalboards/TMC2100_eval.py
@@ -10,7 +10,7 @@ class TMC2100_eval(TMC2100):
     """
     This class represents a TMC2100 Evaluation board.
 
-    Communication is done over the TMCL commands writeMC and readMC. An
+    Communication is done over the TMCL commands writeDRV and readDRV. An
     implementation without TMCL may still use this class if these two functions
     are provided properly. See __init__ for details on the function
     requirements.
@@ -23,13 +23,13 @@ class TMC2100_eval(TMC2100):
                 Type: class
                 A class that provides the neccessary functions for communicating
                 with a TMC2100. The required functions are
-                    connection.writeMC(registerAddress, value, moduleID)
-                    connection.readMC(registerAddress, moduleID, signed)
+                    connection.writeDRV(registerAddress, value, moduleID)
+                    connection.readDRV(registerAddress, moduleID, signed)
                 for writing/reading to registers of the TMC2100.
             moduleID:
                 Type: int, optional, default value: 1
                 The TMCL module ID of the TMC2100. This ID is used as a
-                parameter for the writeMC and readMC functions.
+                parameter for the writeDRV and readDRV functions.
         """
         TMC2100.__init__(self, moduleID)
 
@@ -38,20 +38,20 @@ class TMC2100_eval(TMC2100):
         
         self.APs = _APs
 
-    # Use the motion controller functions for register access
+    # Use the driver controller functions for register access
     def writeRegister(self, registerAddress, value, moduleID=None):
         # If the moduleID argument is omitted, use the stored module ID
         if not moduleID:
             moduleID = self._MODULE_ID
 
-        return self.__connection.writeMC(registerAddress, value, moduleID)
+        return self.__connection.writeDRV(registerAddress, value, moduleID)
 
     def readRegister(self, registerAddress, moduleID=None, signed=False):
         # If the moduleID argument is omitted, use the stored module ID
         if not moduleID:
             moduleID = self._MODULE_ID
 
-        return self.__connection.readMC(registerAddress, moduleID, signed)
+        return self.__connection.readDRV(registerAddress, moduleID, signed)
 
     # Axis parameter access
     def getAxisParameter(self, apType, axis):
