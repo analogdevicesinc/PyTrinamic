ef534ec9be027095d190f3f57150c2d6c45ad243
JM <j.marsollek@web.de>
2019-11-07 13:11:04 +0100

    Added Axisparameters to the TMC4671_eval.py

--- PyTrinamic/evalboards/TMC4671_eval.py
+++ PyTrinamic/evalboards/TMC4671_eval.py
@@ -35,3 +35,23 @@ class TMC4671_eval(TMC4671):
         if channel != 0:
             raise ValueError
         return self.connection.readMC(registerAddress, signed=signed)
+
+class _APs():
+    MaxVelocity = 4
+    Acceleration = 11
+    EnableRamp = 12
+    RampVelocity = 13
+    TargetTorque = 171
+    PID_FLUX_TARGET = 172
+    PID_VELOCITY_TARGET = 173
+    TargetPosition = 174
+    ActualTorque = 176
+    ActualVelocity = 178
+    ActualPosition = 179
+    TargetTorqueRaw = 189
+    PIDIN_TARGET_FLUX = 191
+    TargetVelocity = 192
+    torqueMeasurementFactor = 251
+    StartEncoderInitialization = 252
+    EncoderInitState = 253
+    ActualEncoderWaitTime = 254
