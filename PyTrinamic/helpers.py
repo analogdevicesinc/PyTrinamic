4c0385c8a69b94eb286d3fbc82f005fc7b093dd7
JM <j.marsollek@web.de>
2020-02-03 11:17:49 +0100

    Deleted outdated lines in helpers.py

--- PyTrinamic/helpers.py
+++ PyTrinamic/helpers.py
@@ -6,10 +6,6 @@ Created on 09.01.2019
 
 from PyTrinamic import name, desc
 
-def v_func(verbosity, minimum, func):
-    if(verbosity >= minimum):
-        func()
-
 class TMC_helpers(object):
 
     @staticmethod
@@ -28,58 +24,3 @@ class TMC_helpers(object):
     @staticmethod
     def showInfo():
         print(name + " - " + desc)
-
-    @staticmethod
-    def getComPort(name, return_default=True, CAN=False, Serial=False, USB=False):
-        ports = [p for p in getAvailableComPorts(CAN, Serial, USB) if str(p) == name]
-        if(ports):
-            return ports[0]
-        return (firstAvailableComPort(CAN, Serial, USB) if return_default else None)
-
-    # Print all available Ports
-    @staticmethod
-    def showAvailableComPorts(CAN=False, Serial=False, USB=False):
-        ports = getAvailableComPorts(CAN, Serial, USB)
-        print("Available COM ports: " + str(ports))
-
-    # Return a list of all available ports of the selected interfaces
-    @staticmethod
-    def getAvailableComPorts(CAN=False, Serial=False, USB=False):
-        if CAN == False and Serial == False and USB == False:
-            raise ValueError()
-
-        connected = []
-
-        if CAN:
-            connected = connected + getAvailableCANPorts()
-
-        if Serial:
-            connected = connected + getAvailableSerialPorts()
-
-        if USB:
-            connected = connected + getAvailableUSBPorts()
-
-        return sorted(list(set(connected)))
-
-    # Return a list of all available serial ports
-    @staticmethod
-    def getAvailableSerialPorts():
-        return serial_tmcl_interface.list()
-
-    # Return a list of all available USB ports with correct Vendor and Product IDs
-    @staticmethod
-    def getAvailableUSBPorts():
-        return usb_tmcl_interface.list()
-
-    # Return a list of all available CAN ports
-    @staticmethod
-    def getAvailableCANPorts():
-        return pcan_tmcl_interface.list()
-
-    @staticmethod
-    def firstAvailableComPort(CAN=False, Serial=False, USB=False):
-        ports = getAvailableComPorts(CAN, Serial, USB)
-
-        if len(ports) > 0:
-            return ports[0]
-        return None
