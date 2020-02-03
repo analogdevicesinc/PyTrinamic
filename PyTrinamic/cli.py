10ee39dec4155289caf884a3d6b7df9846299202
JM <j.marsollek@web.de>
2020-02-03 11:14:37 +0100

    Outdated , look at the Connection Manager

--- PyTrinamic/cli.py
+++
@@ -1,9 +0,0 @@
-import PyTrinamic
-
-def select_com_port_by_name(CAN=False, Serial=False, USB=False):
-    PyTrinamic.showAvailableComPorts(CAN, Serial, USB)
-    return input("Select COM port by name: ")
-
-# TODO
-def select_com_port_by_id(CAN=False, Serial=False, USB=False):
-    pass
