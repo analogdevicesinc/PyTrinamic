712f9d31e9c4aacb6838c2c1c8d4b8e5d9966827
JM <j.marsollek@web.de>
2020-02-07 16:10:25 +0100

    Changed TMC-2590 Example

--- PyTrinamic/examples/evalboards/TMC2590/TMC2590_eval_rotateDemo.py
+++ PyTrinamic/examples/evalboards/TMC2590/TMC2590_eval_rotateDemo.py
@@ -5,32 +5,25 @@ Created on 07.02.2020
 @author: JM
 '''
 
-if __name__ == '__main__':
-    pass
-
 import time
+import PyTrinamic
 from PyTrinamic.connections.ConnectionManager import ConnectionManager
 from PyTrinamic.evalboards.TMC2590_eval import TMC2590_eval
 
 connectionManager = ConnectionManager()
 myInterface = connectionManager.connect()
 
-# Create an TMC2590-Eval class which communicates over the Landungsbruecke via TMCL
+PyTrinamic.showInfo()
+
 TMC2590 = TMC2590_eval(myInterface)
-
-" read ChipInfo "
-
 TMC2590.showChipInfo()
 
 DEFAULT_MOTOR = 0
 
-#TMC2590.setAxisParameter(TMC2590.APs.MaxVelocity,     DEFAULT_MOTOR, 1000)
-TMC2590.setAxisParameter(TMC2590.APs.MaxAcceleration, DEFAULT_MOTOR, 10000)
-
 print("Rotating")
-TMC2590.rotate(DEFAULT_MOTOR, 30*25600)
+TMC2590.rotate(DEFAULT_MOTOR, 10*25600)
 
-time.sleep(10);
+time.sleep(2);
 
 print("Stopping")
 TMC2590.stop(DEFAULT_MOTOR)
@@ -38,12 +31,12 @@ TMC2590.stop(DEFAULT_MOTOR)
 time.sleep(1);
 
 print("Moving back to 0")
-TMC2590.moveTo(DEFAULT_MOTOR, 0, 30*25600)
+TMC2590.moveTo(DEFAULT_MOTOR, 0, 10*25600)
  
 # Wait until position 0 is reached
 while TMC2590.getAxisParameter(TMC2590.APs.ActualPosition, DEFAULT_MOTOR) != 0:
     pass
 
-print("Reached Position 0") 
+print("Reached Position 0")
 
 myInterface.close()
