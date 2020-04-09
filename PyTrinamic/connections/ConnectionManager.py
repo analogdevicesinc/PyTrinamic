fa69a3240bfe8da0831ab798aa65a0920687e264
Jonathan Hammer <mail@jhammer.de>
2020-04-09 17:38:30 +0200

    revert file

--- PyTrinamic/connections/ConnectionManager.py
+++ PyTrinamic/connections/ConnectionManager.py
@@ -10,11 +10,9 @@ import argparse
 from PyTrinamic.connections.dummy_tmcl_interface import dummy_tmcl_interface
 from PyTrinamic.connections.pcan_tmcl_interface import pcan_tmcl_interface
 from PyTrinamic.connections.socketcan_tmcl_interface import socketcan_tmcl_interface
-from PyTrinamic.connections.kvaser_tmcl_interface import kvaser_tmcl_interface
 from PyTrinamic.connections.serial_tmcl_interface import serial_tmcl_interface
 from PyTrinamic.connections.uart_ic_interface import uart_ic_interface
 from PyTrinamic.connections.usb_tmcl_interface import usb_tmcl_interface
-from PyTrinamic.connections.slcan_tmcl_interface import slcan_tmcl_interface
 
 class ConnectionManager():
     """
@@ -85,8 +83,6 @@ class ConnectionManager():
         ("dummy_tmcl",      dummy_tmcl_interface,       0),
         ("pcan_tmcl",       pcan_tmcl_interface,        1000000),
         ("socketcan_tmcl",  socketcan_tmcl_interface,   1000000),
-        ("kvaser_tmcl",     kvaser_tmcl_interface,      1000000),
-        ("slcan_tmcl",      slcan_tmcl_interface,       1000000),
         ("serial_tmcl",     serial_tmcl_interface,      9600),
         ("uart_ic",         uart_ic_interface,          9600),
         ("usb_tmcl",        usb_tmcl_interface,         115200)
@@ -94,8 +90,7 @@ class ConnectionManager():
 
     def __init__(self, argList=None, debug=False):
         parser = argparse.ArgumentParser(description='ConnectionManager to setup connections dynamically and interactively')
-        parser.add_argument('--interface', dest='interface', action='store', nargs=1, type=str, choices=['dummy_tmcl', 'pcan_tmcl','kvaser_tmcl'
-        , 'socketcan_tmcl', 'serial_tmcl', 'uart_ic', 'usb_tmcl','slcan_tmcl'], default=['usb_tmcl'],
+        parser.add_argument('--interface', dest='interface', action='store', nargs=1, type=str, choices=['dummy_tmcl', 'pcan_tmcl', 'socketcan_tmcl', 'serial_tmcl', 'uart_ic', 'usb_tmcl'], default=['usb_tmcl'],
                             help='Connection interface (default: %(default)s)')
         parser.add_argument('--port', dest='port', action='store', nargs=1, type=str, default=['any'],
                             help='Connection port (default: %(default)s, n: Use n-th available port, "any": Use any available port, "interactive": Interactive dialogue for port selection, String: Attempt to use the provided string - e.g. COM6 or /dev/tty3)')
