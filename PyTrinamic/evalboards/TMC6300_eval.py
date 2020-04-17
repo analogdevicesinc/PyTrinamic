aedf4204bbcd9006e2e69d29fcbb8299eb4049bd
JM <j.marsollek@web.de>
2020-04-17 14:22:10 +0200

    Added the TMC-6300 IC and EVAL

---
+++ PyTrinamic/evalboards/TMC6300_eval.py
@@ -0,0 +1,68 @@
+'''
+Created on 17.04.2020
+
+@author: JM
+'''
+
+from PyTrinamic.ic.TMC6300.TMC6300 import TMC6300
+
+class TMC6300_eval(TMC6300):
+    def __init__(self, connection, moduleID=1):
+
+        TMC6300.__init__(self, moduleID)
+
+        self.__connection = connection
+        self._MODULE_ID = moduleID
+
+        self.APs = _APs
+
+    # Axis parameter access
+    def getAxisParameter(self, apType, axis):
+        if not(0 <= axis < self.MOTORS):
+            raise ValueError("Axis index out of range")
+
+        return self.__connection.axisParameter(apType, axis)
+
+    def setAxisParameter(self, apType, axis, value):
+        if not(0 <= axis < self.MOTORS):
+            raise ValueError("Axis index out of range")
+
+        self.__connection.setAxisParameter(apType, axis, value)
+
+    # Motion Control functions
+    def rotate(self, motor, value):
+        if not(0 <= motor < self.MOTORS):
+            raise ValueError
+
+        self.__connection.rotate(motor, value, moduleID=self._MODULE_ID)
+
+    def setTargetPWM(self, motor, value):
+        self.setAxisParameter(self.APs.TargetPWM, motor, value)
+
+    def stop(self, motor):
+        self.__connection.stop(motor, moduleID=self._MODULE_ID)
+
+    def setCommutationMode(self, motor, value):
+        self.setAxisParameter(self.APs.CommutationMode, motor, value)
+
+    def ICStandby(self, motor, value):
+        self.setAxisParameter(self.APs.ICStandby, motor, value)
+
+    def setHallDirection(self, motor, value):
+        self.setAxisParameter(self.APs.HallDirection, motor, value)
+
+    def setHallOrder(self, motor, value):
+        self.setAxisParameter(self.APs.HallOrder, motor, value)
+
+class _APs():
+    ActualControlledAngle          = 1
+    ActualHallAngle                = 2
+    ActualPWM                      = 3
+    TargetPWM                      = 4
+    CommutationMode                = 5
+    OpenLoopStepTime               = 6
+    Current                        = 7
+    HallDirection                  = 8
+    HallOrder                      = 9
+    ICStandby                      = 10
+    VMMeasurement                  = 11
---
+++ PyTrinamic/examples/evalboards/TMC6300/TMC6300_hall_Demo.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+'''
+Move a motor back and forth using velocity and position mode of the TMC6300
+
+Created on 17.04.2020
+
+@author: JM
+'''
+
+import time
+import PyTrinamic
+from PyTrinamic.connections.ConnectionManager import ConnectionManager
+from PyTrinamic.evalboards.TMC6300_eval import TMC6300_eval
+
+connectionManager = ConnectionManager()
+myInterface = connectionManager.connect()
+
+PyTrinamic.showInfo()
+
+TMC6300 = TMC6300_eval(myInterface)
+TMC6300.showChipInfo()
+
+DEFAULT_MOTOR = 0
+
+TMC6300.ICStandby(DEFAULT_MOTOR, 0)
+TMC6300.setCommutationMode(DEFAULT_MOTOR, 1)
+TMC6300.setHallDirection(DEFAULT_MOTOR, 1)
+TMC6300.setHallOrder(DEFAULT_MOTOR, 0)
+
+print()
+print("Begin Hall demo")
+print("Rotating")
+TMC6300.setTargetPWM(DEFAULT_MOTOR, 6000)
+
+time.sleep(3);
+
+print("Stopping")
+TMC6300.setTargetPWM(DEFAULT_MOTOR, 0)
+
+time.sleep(2)
+
+print("Rotating back")
+TMC6300.setTargetPWM(DEFAULT_MOTOR, -6000)
+
+time.sleep(3);
+
+print("Stopping")
+TMC6300.setTargetPWM(DEFAULT_MOTOR, 0)
+
+myInterface.close()
