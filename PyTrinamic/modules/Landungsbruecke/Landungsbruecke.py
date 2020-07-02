35e1a5db5cd78136a1a4e4e3c13d80195eab28cd
LH <hess@trinamic.com>
2020-07-02 12:15:33 +0200

    Landungsbruecke: Added ID detection support

--- PyTrinamic/modules/Landungsbruecke/Landungsbruecke.py
+++ PyTrinamic/modules/Landungsbruecke/Landungsbruecke.py
@@ -4,6 +4,8 @@ Created on 24.07.2019
 @author: LK
 '''
 
+from PyTrinamic.TMCL import TMCL_Command
+
 class _GPs():
     VitalSignsErrorMask  = 1
     DriversEnable        = 2
@@ -16,3 +18,92 @@ class Landungsbruecke():
     def __init__(self, connection):
         self.GPs   = _GPs
         self.__connection = connection
+
+        self._mcId = 0
+        self._drvId = 0
+
+    def getBoardIDs(self):
+        '''
+        Read out the IDs of the detected boards.
+
+        This does not start a detection.
+        Returns a tuple of IDs: (drvId, mcId)
+        '''
+        value = self.__connection.globalParameter(self.GPs.BoardAssignment, 0)
+
+        drvStatus = (value >> 24) & 0xFF
+        drvId     = (value >> 16) & 0xFF
+        mcStatus  = (value >>  8) & 0xFF
+        mcId      = (value      ) & 0xFF
+
+        if (mcStatus == 2):
+            self._mcId = mcId
+        if (drvStatus == 2):
+            self._drvId = drvId
+
+        return (self._mcId, self._drvId)
+
+    def detectBoardIDs(self):
+        '''
+        Start an IDDetection and read out the IDs of the detected boards.
+        '''
+
+        while not self.__connection.send(TMCL_Command.ASSIGNMENT, 0, 0, 0).isValid():
+            pass
+
+        return self.getBoardIDs()
+
+    def getBoardNames(self):
+        boardIDs = self.getBoardIDs()
+
+        try:
+            mcName = self.mcIdNames[boardIDs[0]]
+        except KeyError:
+            mcName = str(boardIDs[0])
+
+        try:
+            drvName = self.drvIdNames[boardIDs[1]]
+        except KeyError:
+            drvName = str(boardIDs[1])
+
+        return (mcName, drvName)
+
+    mcIdNames = {
+        0  : "None",
+        2  : "TMC5031",
+        4  : "TMC4361",
+        5  : "TMC5130",
+        6  : "TMC5041",
+        7  : "TMC5072",
+        9  : "TMC4670",
+        10 : "TMC4331",
+        11 : "TMC4361A",
+        13 : "TMC4671",
+        15 : "TMC4330",
+        16 : "TMC5160",
+        18 : "TMC5161",
+        25 : "TMC5062",
+        26 : "TMC8461",
+        27 : "TMC8462",
+    }
+
+    drvIdNames = {
+        0  : "None",
+        1  : "TMC2660",
+        3  : "TMC2130",
+        4  : "TMC2100",
+        5  : "TMC2041",
+        6  : "TMC2208",
+        7  : "TMC2224",
+        8  : "TMC2209",
+        9  : "TMCC160",
+        10 : "TMC6200",
+        11 : "TMC2160",
+        12 : "TMC7300",
+        13 : "TMC2590",
+        18 : "TMC2225",
+        19 : "TMC6100",
+        14 : "TMC2300",
+        21 : "TMC6300",
+        22 : "TMC2226",
+    }
