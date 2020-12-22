048d161ffd91fccb6083e9a847ba47fee0049ddc
Leonard Kugis <leonard@kug.is>
2020-12-22 19:13:04 +0100

    Optimized performance tests

--- PyTrinamic/tests/performance/latency.py
+++ PyTrinamic/tests/performance/latency.py
@@ -15,7 +15,11 @@ import time
 MODULE_ID = 1
 N_SAMPLES = 10000
 
-results = []
+min_time = 0
+max_time = 0
+avg_time = 0
+std_dev_time = 0
+n = 0
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -32,19 +36,28 @@ logger.info("Initializing interface.")
 interface = ConnectionManagerPC.interactive().connect()[0]
 
 logger.info("Performing test.")
-while(len(results) < N_SAMPLES):
+while(n < N_SAMPLES):
     t = time.time()
     # send invalid (for all modules) TMCL Request
     reply = interface.send_request(TMCL_Request(MODULE_ID, 1, 2, 3, 4, 5))
     delta = (time.time() - t) * 1000
     if(delta < 5):
         logger.debug("Measured delta time: {} ms".format(delta))
-        results.append(delta)
+        value = delta
+        if(n == 0):
+            min_time = value
+            max_time = value
+            avg_time = value
+            std_dev_time = 0
+        else:
+            min_time = min(min_time, value)
+            max_time = max(max_time, value)
+            avg_time_new = avg_time + ((value - avg_time) / (n + 1))
+            std_dev_time = (((n - 1) * std_dev_time) + ((value - avg_time_new) * (value - avg_time))) / n
+            avg_time = avg_time_new
+        n += 1
 
 logger.info("Calculating statistical values.")
-min_time = min(results)
-max_time = max(results)
-avg_time = (sum(results)) / len(results)
-std_dev_time = math.sqrt(sum([((i - avg_time)**2) for i in results]) / (len(results) - 1))
+std_dev_time = math.sqrt(std_dev_time)
 
 logger.info("Minimum: {} ms, Maximum: {} ms, Mean: {} ms, Standard deviation: {} ms".format(min_time, max_time, avg_time, std_dev_time))
--- PyTrinamic/tests/performance/throughput.py
+++ PyTrinamic/tests/performance/throughput.py
@@ -13,11 +13,15 @@ import math
 import time
 
 MODULE_ID = 1
-N_SAMPLES = 1000
-PAYLOAD = 2
+N_SAMPLES = 10000
+PAYLOAD = 1
 WORST_CASE_LATENCY = 50 # ms
 
-results = []
+min_time = 0
+max_time = 0
+avg_time = 0
+std_dev_time = 0
+n = 0
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -34,7 +38,7 @@ logger.info("Initializing interface.")
 interface = ConnectionManagerPC.interactive().connect()[0]
 
 logger.info("Performing test.")
-while(len(results) < N_SAMPLES):
+while(n < N_SAMPLES):
     t = time.time()
     for i in range(0, PAYLOAD):
         interface.send_request_only(TMCL_Request(MODULE_ID, 1, 2, 3, 4, 5))
@@ -46,14 +50,23 @@ while(len(results) < N_SAMPLES):
     delta = time.time() - t
     if(delta < (5 * PAYLOAD)):
         logger.debug("Measured delta time: {} s".format(delta))
-        results.append((2 * PAYLOAD) / delta)
+        value = (2 * PAYLOAD) / delta
+        if(n == 0):
+            min_time = value
+            max_time = value
+            avg_time = value
+            std_dev_time = 0
+        else:
+            min_time = min(min_time, value)
+            max_time = max(max_time, value)
+            avg_time_new = avg_time + ((value - avg_time) / (n + 1))
+            std_dev_time = (((n - 1) * std_dev_time) + ((value - avg_time_new) * (value - avg_time))) / n
+            avg_time = avg_time_new
+        n += 1
     else:
         logger.debug("Timeout, not counting")
 
 logger.info("Calculating statistical values.")
-min_time = min(results)
-max_time = max(results)
-avg_time = sum(results) / len(results)
-std_dev_time = math.sqrt(sum([((i - avg_time)**2) for i in results]) / (len(results) - 1))
+std_dev_time = math.sqrt(std_dev_time)
 
 logger.info("Minimum: {} commands/s, Maximum: {} commands/s, Mean: {} commands/s, Standard deviation: {} commands/s".format(min_time, max_time, avg_time, std_dev_time))
