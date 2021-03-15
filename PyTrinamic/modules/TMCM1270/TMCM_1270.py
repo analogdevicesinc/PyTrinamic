e7cf506e5ed7c6b0f029c26fc2bb1f3029d0d742
Leonard Kugis <leonard@kug.is>
2021-03-15 19:11:14 +0100

    TMCM1270
    Moved GPs, APs and ENUMs from module classes to nested classes.
    This way, no additional explicit assignment in __init__ is neccessary.

--- PyTrinamic/modules/TMCM1270/TMCM_1270.py
+++ PyTrinamic/modules/TMCM1270/TMCM_1270.py
@@ -15,12 +15,105 @@ from PyTrinamic.MotorManager import MotorManager
 
 class TMCM_1270(tmcl_module, LinearRampModule, MotorControl, StallGuard2Module):
 
+    class APs:
+        TargetPosition                 = 0
+        ActualPosition                 = 1
+        TargetVelocity                 = 2
+        ActualVelocity                 = 3
+        MaxVelocity                    = 4
+        MaxAcceleration                = 5
+        MaxCurrent                     = 6
+        StandbyCurrent                 = 7
+        PositionReachedFlag            = 8
+        HomeSwitch                     = 9
+        RightEndstop                   = 10
+        LeftEndstop                    = 11
+        AutomaticRightStop             = 12
+        AutomaticLeftStop              = 13
+        swapSwitchInputs               = 14
+        A1                             = 15
+        V1                             = 16
+        MaxDeceleration                = 17
+        D1                             = 18
+        StartVelocity                  = 19
+        StopVelocity                   = 20
+        RampWaitTime                   = 21
+        THIGH                          = 22
+        VDCMIN                         = 23
+        rightSwitchPolarity            = 24
+        leftSwitchPolarity             = 25
+        softstop                       = 26
+        HighSpeedChopperMode           = 27
+        HighSpeedFullstepMode          = 28
+        MeasuredSpeed                  = 29
+        PowerDownRamp                  = 31
+        RelativePositioningOptionCode  = 127
+        MicrostepResolution            = 140
+        ChopperBlankTime               = 162
+        ConstantTOffMode               = 163
+        DisableFastDecayComparator     = 164
+        ChopperHysteresisEnd           = 165
+        ChopperHysteresisStart         = 166
+        TOff                           = 167
+        SEIMIN                         = 168
+        SECDS                          = 169
+        SmartEnergyHysteresis          = 170
+        SECUS                          = 171
+        SmartEnergyHysteresisStart     = 172
+        SG2FilterEnable                = 173
+        SG2Threshold                   = 174
+        ShortToGroundProtection        = 177
+        VSense                         = 179
+        SmartEnergyActualCurrent       = 180
+        SmartEnergyStallVelocity       = 181
+        SmartEnergyThresholdSpeed      = 182
+        RandomTOffMode                 = 184
+        ChopperSynchronization         = 185
+        PWMThresholdSpeed              = 186
+        PWMGrad                        = 187
+        PWMAmplitude                   = 188
+        PWMScale                       = 189
+        pwmMode                        = 190
+        PWMFrequency                   = 191
+        PWMAutoscale                   = 192
+        ReferenceSearchMode            = 193
+        ReferenceSearchSpeed           = 194
+        RefSwitchSpeed                 = 195
+        RightLimitSwitchPosition       = 196
+        LastReferencePosition          = 197
+        encoderMode                    = 201
+        MotorFullStepResolution        = 202
+        pwmSymmetric                   = 203
+        FreewheelingMode               = 204
+        LoadValue                      = 206
+        extendedErrorFlags             = 207
+        DrvStatusFlags                 = 208
+        EncoderPosition                = 209
+        EncoderResolution              = 210
+        max_EncoderDeviation           = 212
+        PowerDownDelay                 = 214
+        UnitMode                       = 255
+
+    class ENUMs:
+        pass
+
+    class GPs:
+        CANBitrate                    = 69
+        CANSendId                     = 70
+        CANReceiveId                  = 71
+        CANSecondaryId                = 72
+        autoStartMode                 = 77
+        protectionMode                = 81
+        eepromCoordinateStore         = 84
+        zeroUserVariables             = 85
+        applicationStatus             = 128
+        programCounter                = 130
+        lastTmclError                 = 131
+        tickTimer                     = 132
+        randomNumber                  = 133
+
     def __init__(self, connection, module_id=1):
         tmcl_module.__init__(self, connection, module_id)
-
-        self.GPs   = _GPs
-        self.APs   = _APs
-        self.ENUMs = _ENUMs
 
         self.MOTORS = [
             MotorManager.motor(0, [self], features=[LinearRampMotor, MotorControlMotor, StallGuard2Motor])
@@ -88,101 +181,3 @@ class TMCM_1270(tmcl_module, LinearRampModule, MotorControl, StallGuard2Module):
         print("Motion configuration:")
         print("\tMax velocity: " + str(self.getMaxVelocity(axis)))
         print("\tAcceleration: " + str(self.getMaxAcceleration(axis)))
-
-class _APs():
-    TargetPosition                 = 0
-    ActualPosition                 = 1
-    TargetVelocity                 = 2
-    ActualVelocity                 = 3
-    MaxVelocity                    = 4
-    MaxAcceleration                = 5
-    MaxCurrent                     = 6
-    StandbyCurrent                 = 7
-    PositionReachedFlag            = 8
-    HomeSwitch                     = 9
-    RightEndstop                   = 10
-    LeftEndstop                    = 11
-    AutomaticRightStop             = 12
-    AutomaticLeftStop              = 13
-    swapSwitchInputs               = 14
-    A1                             = 15
-    V1                             = 16
-    MaxDeceleration                = 17
-    D1                             = 18
-    StartVelocity                  = 19
-    StopVelocity                   = 20
-    RampWaitTime                   = 21
-    THIGH                          = 22
-    VDCMIN                         = 23
-    rightSwitchPolarity            = 24
-    leftSwitchPolarity             = 25
-    softstop                       = 26
-    HighSpeedChopperMode           = 27
-    HighSpeedFullstepMode          = 28
-    MeasuredSpeed                  = 29
-    PowerDownRamp                  = 31
-    RelativePositioningOptionCode  = 127
-    MicrostepResolution            = 140
-    ChopperBlankTime               = 162
-    ConstantTOffMode               = 163
-    DisableFastDecayComparator     = 164
-    ChopperHysteresisEnd           = 165
-    ChopperHysteresisStart         = 166
-    TOff                           = 167
-    SEIMIN                         = 168
-    SECDS                          = 169
-    SmartEnergyHysteresis          = 170
-    SECUS                          = 171
-    SmartEnergyHysteresisStart     = 172
-    SG2FilterEnable                = 173
-    SG2Threshold                   = 174
-    ShortToGroundProtection        = 177
-    VSense                         = 179
-    SmartEnergyActualCurrent       = 180
-    SmartEnergyStallVelocity       = 181
-    SmartEnergyThresholdSpeed      = 182
-    RandomTOffMode                 = 184
-    ChopperSynchronization         = 185
-    PWMThresholdSpeed              = 186
-    PWMGrad                        = 187
-    PWMAmplitude                   = 188
-    PWMScale                       = 189
-    pwmMode                        = 190
-    PWMFrequency                   = 191
-    PWMAutoscale                   = 192
-    ReferenceSearchMode            = 193
-    ReferenceSearchSpeed           = 194
-    RefSwitchSpeed                 = 195
-    RightLimitSwitchPosition       = 196
-    LastReferencePosition          = 197
-    encoderMode                    = 201
-    MotorFullStepResolution        = 202
-    pwmSymmetric                   = 203
-    FreewheelingMode               = 204
-    LoadValue                      = 206
-    extendedErrorFlags             = 207
-    DrvStatusFlags                 = 208
-    EncoderPosition                = 209
-    EncoderResolution              = 210
-    max_EncoderDeviation           = 212
-    PowerDownDelay                 = 214
-    UnitMode                       = 255
-
-class _ENUMs():
-    pass
-
-class _GPs():
-
-    CANBitrate                    = 69
-    CANSendId                     = 70
-    CANReceiveId                  = 71
-    CANSecondaryId                = 72
-    autoStartMode                 = 77
-    protectionMode                = 81
-    eepromCoordinateStore         = 84
-    zeroUserVariables             = 85
-    applicationStatus             = 128
-    programCounter                = 130
-    lastTmclError                 = 131
-    tickTimer                     = 132
-    randomNumber                  = 133
