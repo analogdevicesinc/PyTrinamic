c205aa252726ec93e7e134c6459e2badb8d0d0f7
Leonard Kugis <leonard@kug.is>
2021-03-24 22:01:26 +0100

    TMCM_3110: Inheriting abstract module class and fixed axis arguments.
    Moved APs, GPs and ENUMs from module classes to nested class within TMCM_3110.
    This way, no additional assignment in __init__ is required.

--- PyTrinamic/modules/TMCM3110/TMCM_3110.py
+++ PyTrinamic/modules/TMCM3110/TMCM_3110.py
@@ -4,204 +4,185 @@ Created on 05.06.2020
 @author: JM
 '''
 
-class TMCM_3110():
+from PyTrinamic.modules.tmcl_module import tmcl_module
+
+class TMCM_3110(tmcl_module):
     MOTORS = 3
 
-    def __init__(self, connection):
-        self.connection = connection
-
-        #self.GPs   = _GPs
-        self.APs   = _APs
-        #self.ENUMs = _ENUMs
+    class APs():
+        TargetPosition                 = 0
+        ActualPosition                 = 1
+        TargetVelocity                 = 2
+        ActualVelocity                 = 3
+        MaxVelocity                    = 4
+        MaxAcceleration                = 5
+        MaxCurrent                     = 6
+        StandbyCurrent                 = 7
+        PositionReachedFlag            = 8
+        ReferenceSwitchStatus          = 9
+        RightEndstop                   = 10
+        LeftEndstop                    = 11
+        RightLimitSwitchDisable        = 12
+        LeftLimitSwitchDisable         = 13
+        MinimumSpeed                   = 130
+        ActualAcceleration             = 135
+        RampMode                       = 138
+        MicrostepResolution            = 140
+        ReferenceSwitchTolerance       = 141
+        SoftStopFlag                   = 149
+        EndSwitchPowerDown             = 150
+        RampDivisor                    = 153
+        PulseDivisor                   = 154
+        Intpol                         = 160
+        DoubleEdgeSteps                = 161
+        ChopperBlankTime               = 162
+        ConstantTOffMode               = 163
+        DisableFastDecayComparator     = 164
+        ChopperHysteresisEnd           = 165
+        ChopperHysteresisStart         = 166
+        TOff                           = 167
+        SEIMIN                         = 168
+        SECDS                          = 169
+        smartEnergyHysteresis          = 170
+        SECUS                          = 171
+        smartEnergyHysteresisStart     = 172
+        SG2FilterEnable                = 173
+        SG2Threshold                   = 174
+        SlopeControlHighSide           = 175
+        SlopeControlLowSide            = 176
+        ShortToGroundProtection        = 177
+        ShortDetectionTime             = 178
+        VSense                         = 179
+        smartEnergyActualCurrent       = 180
+        smartEnergyStallVelocity       = 181
+        smartEnergyThresholdSpeed      = 182
+        smartEnergySlowRunCurrent      = 183
+        RandomTOffMode                 = 184
+        ReferenceSearchMode            = 193
+        ReferenceSearchSpeed           = 194
+        ReferenceSwitchSpeed           = 195
+        ReferenceSwitchDistance        = 196
+        LastReferenceSwitchPosition    = 197
+        BoostCurrent                   = 200
+        EncoderMode                    = 201
+        FreewheelingDelay              = 204
+        LoadValue                      = 206
+        ExtendedErrorFlags             = 207
+        DrvStatusFlags                 = 208
+        EncoderPosition                = 209
+        EncoderPrescaler               = 210
+        MaxEncoderDeviation            = 212
+        GroupIndex                     = 213
+        PowerDownDelay                 = 214
+        StepDirectionMode              = 254
 
     @staticmethod
     def getEdsFile():
         return __file__.replace("TMCM_3110.py", "TMCM_3110_V.320.eds")
 
     def showChipInfo(self):
-        ("The TMCM-3110 is a 3-Axis Stepper Controller / Driver. Voltage supply: 12 - 48V");
-
-    # Axis parameter access
-    def getAxisParameter(self, apType, motor):
-        return self.connection.axisParameter(apType, motor)
-
-    def setAxisParameter(self, apType, motor, value):
-        self.connection.setAxisParameter(apType, motor, value)
-
-    # Global parameter access
-    def getGlobalParameter(self, gpType, bank):
-        return self.connection.globalParameter(gpType, bank)
-
-    def setGlobalParameter(self, gpType, bank, value):
-        self.connection.setGlobalParameter(gpType, bank, value)
+        print("The TMCM-3110 is a 3-Axis Stepper Controller / Driver. Voltage supply: 12 - 48V")
 
     # Motion Control functions
-    def rotate(self, motor, velocity):
-        self.setRampMode(motor, 2)
+    def rotate(self, axis, velocity):
+        self.setRampMode(axis, 2)
 
-        self.setAxisParameter(self.APs.TargetVelocity, motor, velocity)
+        self.setAxisParameter(self.APs.TargetVelocity, axis, velocity)
 
-    def stop(self, motor):
-        self.rotate(motor, 0)
+    def stop(self, axis):
+        self.rotate(axis, 0)
 
-    def moveTo(self, motor, position, velocity=None):
+    def moveTo(self, axis, position, velocity=None):
         if velocity:
-            self.setMaxVelocity(motor, velocity)
+            self.setMaxVelocity(axis, velocity)
 
-        self.setTargetPosition(motor, position)
+        self.setTargetPosition(axis, position)
 
-        self.setRampMode(motor, 0)
+        self.setRampMode(axis, 0)
 
-    def moveBy(self, motor, difference, velocity=None):
-        position = difference + self.getActualPosition(motor)
+    def moveBy(self, axis, difference, velocity=None):
+        position = difference + self.getActualPosition(axis)
 
-        self.moveTo(motor, position, velocity)
+        self.moveTo(axis, position, velocity)
 
         return position
 
     # Current control functions
-    def setMotorRunCurrent(self, motor, current):
-        self.setMaxCurrent(motor, current)
+    def setMotorRunCurrent(self, axis, current):
+        self.setMaxCurrent(axis, current)
 
-    def setMotorStandbyCurrent(self, motor, current):
-        self.setAxisParameter(self.APs.StandbyCurrent, motor, current)
+    def setMotorStandbyCurrent(self, axis, current):
+        self.setAxisParameter(self.APs.StandbyCurrent, axis, current)
 
-    def getMaxCurrent(self, motor):
-        return self.getAxisParameter(self.APs.MaxCurrent, motor)
+    def getMaxCurrent(self, axis):
+        return self.getAxisParameter(self.APs.MaxCurrent, axis)
 
-    def setMaxCurrent(self, motor, current):
-        self.setAxisParameter(self.APs.MaxCurrent, motor, current)
+    def setMaxCurrent(self, axis, current):
+        self.setAxisParameter(self.APs.MaxCurrent, axis, current)
 
     # StallGuard2 Functions
-    def setStallguard2Filter(self, motor, enableFilter):
-        self.setAxisParameter(self.APs.SG2FilterEnable, motor, enableFilter)
+    def setStallguard2Filter(self, axis, enableFilter):
+        self.setAxisParameter(self.APs.SG2FilterEnable, axis, enableFilter)
 
-    def setStallguard2Threshold(self, motor, threshold):
-        self.setAxisParameter(self.APs.SG2Threshold, motor, threshold)
+    def setStallguard2Threshold(self, axis, threshold):
+        self.setAxisParameter(self.APs.SG2Threshold, axis, threshold)
 
-    def setStopOnStallVelocity(self, motor, velocity):
-        self.setAxisParameter(self.APs.smartEnergyStallVelocity, motor, velocity)
+    def setStopOnStallVelocity(self, axis, velocity):
+        self.setAxisParameter(self.APs.smartEnergyStallVelocity, axis, velocity)
 
     # Motion parameter functions
-    def getTargetPosition(self, motor):
-        return self.getAxisParameter(self.APs.TargetPosition, motor)
+    def getTargetPosition(self, axis):
+        return self.getAxisParameter(self.APs.TargetPosition, axis)
 
-    def setTargetPosition(self, motor, position):
-        self.setAxisParameter(self.APs.TargetPosition, motor, position)
+    def setTargetPosition(self, axis, position):
+        self.setAxisParameter(self.APs.TargetPosition, axis, position)
 
-    def getActualPosition(self, motor):
-        return self.getAxisParameter(self.APs.ActualPosition, motor)
+    def getActualPosition(self, axis):
+        return self.getAxisParameter(self.APs.ActualPosition, axis)
 
-    def setActualPosition(self, motor, position):
-        return self.setAxisParameter(self.APs.ActualPosition, motor, position)
+    def setActualPosition(self, axis, position):
+        return self.setAxisParameter(self.APs.ActualPosition, axis, position)
 
-    def getTargetVelocity(self, motor):
-        return self.getAxisParameter(self.APs.TargetVelocity, motor)
+    def getTargetVelocity(self, axis):
+        return self.getAxisParameter(self.APs.TargetVelocity, axis)
 
-    def setTargetVelocity(self, velocity, motor):
-        self.setAxisParameter(self.APs.TargetVelocity, motor, velocity)
+    def setTargetVelocity(self, velocity, axis):
+        self.setAxisParameter(self.APs.TargetVelocity, axis, velocity)
 
-    def getActualVelocity(self, motor):
-        return self.getAxisParameter(self.APs.ActualVelocity, motor)
+    def getActualVelocity(self, axis):
+        return self.getAxisParameter(self.APs.ActualVelocity, axis)
 
-    def getMaxVelocity(self, motor):
-        return self.getAxisParameter(self.APs.MaxVelocity, motor)
+    def getMaxVelocity(self, axis):
+        return self.getAxisParameter(self.APs.MaxVelocity, axis)
 
-    def setMaxVelocity(self, motor, velocity):
-        self.setAxisParameter(self.APs.MaxVelocity, motor, velocity)
+    def setMaxVelocity(self, axis, velocity):
+        self.setAxisParameter(self.APs.MaxVelocity, axis, velocity)
 
-    def getMaxAcceleration(self, motor):
-        return self.getAxisParameter(self.APs.MaxAcceleration, motor)
+    def getMaxAcceleration(self, axis):
+        return self.getAxisParameter(self.APs.MaxAcceleration, axis)
 
-    def setMaxAcceleration(self, motor, acceleration):
-        self.setAxisParameter(self.APs.MaxAcceleration, motor, acceleration)
+    def setMaxAcceleration(self, axis, acceleration):
+        self.setAxisParameter(self.APs.MaxAcceleration, axis, acceleration)
 
-    def getRampMode(self, motor):
-        return self.getAxisParameter(self.APs.RampMode, motor)
+    def getRampMode(self, axis):
+        return self.getAxisParameter(self.APs.RampMode, axis)
 
-    def setRampMode(self, motor, mode):
-        return self.setAxisParameter(self.APs.RampMode, motor, mode)
+    def setRampMode(self, axis, mode):
+        return self.setAxisParameter(self.APs.RampMode, axis, mode)
 
     # Status functions
-    def getStatusFlags(self, motor):
-        return self.getAxisParameter(self.APs.DrvStatusFlags, motor)
+    def getStatusFlags(self, axis):
+        return self.getAxisParameter(self.APs.DrvStatusFlags, axis)
 
-    def getErrorFlags(self, motor):
-        return self.getAxisParameter(self.APs.ExtendedErrorFlags, motor)
+    def getErrorFlags(self, axis):
+        return self.getAxisParameter(self.APs.ExtendedErrorFlags, axis)
 
-    def positionReached(self, motor):
-        return self.getAxisParameter(self.APs.PositionReachedFlag, motor)
+    def positionReached(self, axis):
+        return self.getAxisParameter(self.APs.PositionReachedFlag, axis)
 
     # IO pin functions
     def analogInput(self, x):
-        return self.connection.analogInput(x)
+        return self.connection.analogInput(x, self.module_id)
 
     def digitalInput(self, x):
-        return self.connection.digitalInput(x)
-
-class _APs():
-    TargetPosition                 = 0
-    ActualPosition                 = 1
-    TargetVelocity                 = 2
-    ActualVelocity                 = 3
-    MaxVelocity                    = 4
-    MaxAcceleration                = 5
-    MaxCurrent                     = 6
-    StandbyCurrent                 = 7
-    PositionReachedFlag            = 8
-    ReferenceSwitchStatus          = 9
-    RightEndstop                   = 10
-    LeftEndstop                    = 11
-    RightLimitSwitchDisable        = 12
-    LeftLimitSwitchDisable         = 13
-    MinimumSpeed                   = 130
-    ActualAcceleration             = 135
-    RampMode                       = 138
-    MicrostepResolution            = 140
-    ReferenceSwitchTolerance       = 141
-    SoftStopFlag                   = 149
-    EndSwitchPowerDown             = 150
-    RampDivisor                    = 153
-    PulseDivisor                   = 154
-    Intpol                         = 160
-    DoubleEdgeSteps                = 161
-    ChopperBlankTime               = 162
-    ConstantTOffMode               = 163
-    DisableFastDecayComparator     = 164
-    ChopperHysteresisEnd           = 165
-    ChopperHysteresisStart         = 166
-    TOff                           = 167
-    SEIMIN                         = 168
-    SECDS                          = 169
-    smartEnergyHysteresis          = 170
-    SECUS                          = 171
-    smartEnergyHysteresisStart     = 172
-    SG2FilterEnable                = 173
-    SG2Threshold                   = 174
-    SlopeControlHighSide           = 175
-    SlopeControlLowSide            = 176
-    ShortToGroundProtection        = 177
-    ShortDetectionTime             = 178
-    VSense                         = 179
-    smartEnergyActualCurrent       = 180
-    smartEnergyStallVelocity       = 181
-    smartEnergyThresholdSpeed      = 182
-    smartEnergySlowRunCurrent      = 183
-    RandomTOffMode                 = 184
-    ReferenceSearchMode            = 193
-    ReferenceSearchSpeed           = 194
-    ReferenceSwitchSpeed           = 195
-    ReferenceSwitchDistance        = 196
-    LastReferenceSwitchPosition    = 197
-    BoostCurrent                   = 200
-    EncoderMode                    = 201
-    FreewheelingDelay              = 204
-    LoadValue                      = 206
-    ExtendedErrorFlags             = 207
-    DrvStatusFlags                 = 208
-    EncoderPosition                = 209
-    EncoderPrescaler               = 210
-    MaxEncoderDeviation            = 212
-    GroupIndex                     = 213
-    PowerDownDelay                 = 214
-    StepDirectionMode              = 254
+        return self.connection.digitalInput(x, self.module_id)
