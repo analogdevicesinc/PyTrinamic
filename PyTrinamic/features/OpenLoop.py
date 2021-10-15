c1acb80b1684a81c1d073313c7c21711202b1302
Jonathan Hammer <mail@jhammer.de>
2021-10-15 14:50:05 +0200

    Added OpenLoop feature for setting open loop current

--- PyTrinamic/features/OpenLoop.py
+++ PyTrinamic/features/OpenLoop.py
@@ -9,9 +9,23 @@ from PyTrinamic.features.Feature import Feature
 class OpenLoop(Feature):
  
     def set_open_loop_torque(self, torque):
+        """
+        Sets if open loop torque that is used for this axis.
+        This value is stored as OpenLoopCurrent axis parameter.
+
+        Parameters:
+        current: Open Loop Current
+        """
         raise NotImplementedError()
 
     def get_open_loop_torque(self):
+        """
+        Gets open loop torque that is used for this axis.
+        This value is stored as OpenLoopCurrent axis parameter.
+
+        
+        returns: Open loop current
+        """
         raise NotImplementedError()
 
     def __str__(self):
@@ -19,7 +33,6 @@ class OpenLoop(Feature):
                 "OpenLoop",
                 {
                     "open_loop_torque": self.open_loop_torque,
-
                 }
             )
     
--- PyTrinamic/features/OpenLoopModule.py
+++ PyTrinamic/features/OpenLoopModule.py
@@ -10,12 +10,28 @@ from PyTrinamic.features.OpenLoop import OpenLoop
 class OpenLoopModule(OpenLoop,FeatureProvider):
  
     class __GROUPING(OpenLoop,FeatureProvider):
+        def __init__(self, parent):
+                    self.parent = parent
 
-        def set_open_loop_torque(self, torque):
-            self.parent.set_axis_parameter(self.parent.APs.StartCurrent,torque)
+        def set_open_loop_torque(self, current):
+            """
+            Sets if open loop torque that is used for this axis.
+            This value is stored as OpenLoopCurrent axis parameter.
+
+            Parameters:
+            current: Open Loop Current
+            """
+            self.parent.set_axis_parameter(self.parent.APs.OpenLoopCurrent,current)
 
         def get_open_loop_torque(self):
-            return self.parent.get_axis_parameter(self.parent.APs.StartCurrent)
+            """
+            Gets open loop torque that is used for this axis.
+            This value is stored as OpenLoopCurrent axis parameter.
+
+            
+            returns: Open loop current
+            """
+            return self.parent.get_axis_parameter(self.parent.APs.OpenLoopCurrent)
    
         open_loop_torque = property(get_open_loop_torque,set_open_loop_torque)
     
