063b5def8d4040c8d2b0fda247fae3cb655644b0
Jonathan Hammer <mail@jhammer.de>
2021-11-16 13:36:36 +0100

    CoolStepFeature Update

--- PyTrinamic/features/CoolStep.py
+++ PyTrinamic/features/CoolStep.py
@@ -1,90 +1,164 @@
-# Created on: 23.03.2020
-# Author: LK
+# Created on: 04.03.2021
+# Author: JH
 
-import argparse
-import logging
-import time
-
-import PyTrinamic
 from PyTrinamic.features.Feature import Feature
-from PyTrinamic.features.StallGuard import StallGuard
 
 class CoolStep(Feature):
+    "StallGuard2 feature implementation"
 
-    def __init__(self, module, argList=None, logger=logging.getLogger(__name__)):
-        super().__init__(module)
+    def set_current_minimum(self, axis, current):
+        """
+        Sets the smartEnergy current minimum.
 
-        self.__arglist = argList
-        self.__logger = logger
+        Parameters:
+        axis: Axis index.
+        current: smartEnergy current minimum.
+        """
+        raise NotImplementedError()
 
-        parser = argparse.ArgumentParser(description='coolStep calibration')
-        parser.add_argument('-t', '--target-velocity', dest='velocity', action='store', nargs=1, type=int, default=[100000],
-                            help='Target velocity for demonstration on all axes. Default: %(default)s.')
-        parser.add_argument('-c', '--current', dest='current', action='store', nargs=1, type=int, default=[16],
-                            help='Current Scaler value while motor is running. Default: %(default)s.')
-        parser.add_argument('--current-minimum', dest='seimin', action='store', nargs=1, type=int, default=[1], choices=range(0, 1),
-                            help='Minimal current after decreasing. 0: maximum_current / 2, 1: maximum_current / 4. Default: %(default)s.')
-        parser.add_argument('--current-down-step', dest='down', action='store', nargs=1, type=int, default=[2], choices=range(0, 3),
-                            help='Reaction speed for decreasing. 0: React slowly with ramping, 3: React instantly. Default: %(default)s.')
-        parser.add_argument('--current-up-step', dest='up', action='store', nargs=1, type=int, default=[2], choices=range(0, 3),
-                            help='Reaction speed for increasing. 0: React slowly with ramping, 3: React instantly. Default: %(default)s.')
-        parser.add_argument('--hysteresis-width', dest='hwidth', action='store', nargs=1, type=int, default=[4], choices=range(0, 15),
-                            help='CoolStep hysteresis window width in [1/16 SGT_MAX]. Default: %(default)s.')
-        parser.add_argument('--hysteresis-start', dest='hstart', action='store', nargs=1, type=int, default=[9], choices=range(0, 15),
-                            help='CoolStep hysteresis window start in [1/16 SGT_MAX]. Default: %(default)s.')
-        parser.add_argument('--coolstep-threshold', dest='coolstep_threshold', action='store', nargs=1, type=int, default=[0],
-                            help='Velocity threshold, above which CoolStep becomes active. Default: %(default)s.')
-        parser.add_argument('-i', '--interactive', dest='interactive', action='store_true',
-                            help='Interactive CoolStep hysteresis window calibration.')
-        args = parser.parse_known_args(argList)[0]
+    def set_current_down_step(self, axis, step):
+        """
+        Sets the smartEnergy current down step.
 
-        self.__threshold = args.coolstep_threshold[0]
-        self.__interactive = args.interactive
+        Parameters:
+        axis: Axis index.
+        step: smartEnergy current down step.
+        """
+        raise NotImplementedError()
 
-        logger.debug(f"Minimum current: {args.seimin[0]}")
-        logger.debug(f"Current down step: {args.down[0]}")
-        logger.debug(f"Current up step: {args.up[0]}")
-        logger.debug(f"Hysteresis width: {args.hwidth[0]}")
-        logger.debug(f"Hysteresis start: {args.hstart[0]}")
-        logger.debug(f"CoolStep threshold velocity: {self.__threshold}")
-        logger.debug(f"Interactive: {self.__interactive}")
+    def set_current_up_step(self, axis, ):
+        """
+        Sets the smartEnergy current up step.
 
-        logger.info(f"Minimum current: {args.seimin[0]} => {args.current[0] / (1 << args.seimin[0])}")
-        logger.info(f"Hysteresis start: {args.hstart[0]} => {args.hstart[0] * 64}")
-        logger.info(f"Hysteresis width: {args.hwidth[0]} => {args.hwidth[0] * 64}")
-        logger.info(f"Hysteresis end: {args.hstart[0] + args.hwidth[0]} => {(args.hstart[0] + args.hwidth[0]) * 64}")
+        Parameters:
+        axis: Axis index.
+        step: smartEnergy current up step.
+        """
+        raise NotImplementedError()
 
-        self._module.setAxisParameter(self._module.APs.SEIMIN, 0, args.seimin[0])
-        self._module.setAxisParameter(self._module.APs.SECDS, 0, args.down[0])
-        self._module.setAxisParameter(self._module.APs.SECUS, 0, args.up[0])
-        self._module.setAxisParameter(self._module.APs.smartEnergyHysteresis, 0, args.hwidth[0])
-        self._module.setAxisParameter(self._module.APs.smartEnergyHysteresisStart, 0, args.hstart[0])
-        self._module.setAxisParameter(self._module.APs.smartEnergyThresholdSpeed, 0, args.velocity[0] + 1)
+    def set_hysteresis(self, axis, hysteresis):
+        """
+        Sets the smartEnergy hysteresis.
 
-    def calibrate(self):
-        StallGuard(self._module, self.__arglist, self.__logger).calibrate_middle()
+        Parameters:
+        axis: Axis index.
+        hysteresis: smartEnergy hysteresis.
+        """
+        raise NotImplementedError()
 
-        if(self.__interactive):
-            print("Now, apply some load to the motor, at which you want the current to increase automatically.")
-            input("Press enter to continue ...")
+    def set_hysteresis_start(self, axis, hysteresis_start):
+        """
+        Sets the smartEnergy hysteresis start.
 
-            for i in range(3):
-                self.__logger.info(f"Starting calibration in {3-i} seconds.")
-                time.sleep(1.0)
+        Parameters:
+        axis: Axis index.
+        hysteresis_start: smartEnergy hysteresis start .
+        """
+        raise NotImplementedError()
 
-            hstart = int(round((self._module.getAxisParameter(self._module.APs.LoadValue, 0) / 1023) * 15, 0))
-            self.__logger.info(f"Hysteresis start: {hstart}")
+    def set_threshold_speed(self, axis, speed):
+        """
+        Sets the smartEnergy speed threshold.
 
-            print("Now, release the load. Let the motor run freely.")
-            input("Press enter to continue ...")
+        Parameters:
+        axis: Axis index.
+        speed: smartEnergy threshold speed.
+        """
+        raise NotImplementedError()
 
-            hend = int(round((self._module.getAxisParameter(self._module.APs.LoadValue, 0) / 1023) * 15, 0))
-            self.__logger.info(f"Hysteresis end: {hend}")
+    def set_slow_run_current(self, axis, current):
+        """
+        Sets the smartEnergy slow run current.
 
-            hwidth = hend - hstart
-            self.__logger.info(f"Hysteresis window width: {hwidth}")
+        Parameters:
+        axis: Axis index.
+        current: smartEnergy slow run current
+        """
+        raise NotImplementedError()
+    
 
-            self._module.setAxisParameter(self._module.APs.smartEnergyHysteresis, 0, hwidth)
-            self._module.setAxisParameter(self._module.APs.smartEnergyHysteresisStart, 0, hstart)
+    def get_current_minimum(self, axis):
+        """
+        Gets the smartEnergy current minimum.
 
-        self._module.setAxisParameter(self._module.APs.smartEnergyThresholdSpeed, 0, self.__threshold)
+        Returns: 
+        smartEnergy current minimum.
+        """
+        raise NotImplementedError()
+
+    def get_current_down_step(self, axis):
+        """
+        Gets the smartEnergy current down step.
+
+        Returns: 
+        smartEnergy current down step.
+        """
+        raise NotImplementedError()
+
+    def get_current_up_step(self, axis):
+        """
+        Gets the smartEnergy current up step.
+
+        Returns:
+        smartEnergy current up step.
+        """
+        raise NotImplementedError()
+
+    def get_hysteresis(self, axis):
+        """
+        Gets the smartEnergy hysteresis.
+
+        Returns:
+        smartEnergy hysteresis.
+        """
+        raise NotImplementedError()
+
+    def get_hysteresis_start(self, axis):
+        """
+        Gets the smartEnergy hysteresis start.
+
+        Returns:
+        smartEnergy hysteresis start .
+        """
+        raise NotImplementedError()
+
+    def get_threshold_speed(self, axis):
+        """
+        Gets the smartEnergy speed threshold.
+
+        Returns:
+        smartEnergy threshold speed.
+        """
+        raise NotImplementedError()
+
+    def get_slow_run_current(self, axis):
+        """
+        Gets the smartEnergy slow run current.
+        
+        Returns:
+        smartEnergy slow run current
+        """
+        raise NotImplementedError()
+    
+    def __str__(self):
+        return "{} {}".format(
+            "CoolStep",
+            {
+                "current minimum" : self.current_minimum,
+                "current down step": self.current_down_step,
+                "current up step": self.current_up_step,
+                "hysteresis": self.hysteresis,
+                "hysteresis_start": self.hysteresis_start,
+                "threshold_speed": self.threshold_speed,
+                "slow_run_current": self.slow_run_current,
+            }
+        )
+
+    # Properties
+    current_minimum = property(get_current_minimum, set_current_minimum)
+    current_down_step = property(get_current_down_step, set_current_down_step)
+    current_up_step = property(get_current_up_step, set_current_up_step)
+    hysteresis = property(get_hysteresis, set_hysteresis)
+    hysteresis_start = property(get_hysteresis_start, set_hysteresis_start)
+    threshold_speed = property(get_threshold_speed, set_threshold_speed)
+    slow_run_current = property(get_slow_run_current, set_slow_run_current)
