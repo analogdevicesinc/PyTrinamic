6d0e8abc669e484d31ba7d4a0795a98633104ada
Leonard Kugis <leonard@kug.is>
2022-04-05 23:30:53 +0200

    Implemented RAMDebug example for MAX22216_eval

---
+++ examples/evalboards/MAX22216/ramdebug.py
@@ -0,0 +1,25 @@
+import pytrinamic
+from pytrinamic.connections import ConnectionManager
+from pytrinamic.ic import MAX22216
+from pytrinamic.RAMDebug import Channel, RAMDebug, RAMDebug_Trigger
+
+pytrinamic.show_info()
+
+with ConnectionManager(debug=True).connect() as my_interface:
+    print(my_interface)
+
+    ch = Channel.field(0, MAX22216.FIELD.ADC_VM_RAW, signed=True, eval_channel=1)
+    trigger = Channel.field(0, MAX22216.FIELD.ADC_VM_RAW, signed=True, eval_channel=1)
+
+    debug = RAMDebug(my_interface)
+    debug.set_channel(ch)
+    debug.set_trigger_type(RAMDebug_Trigger.TRIGGER_RISING_EDGE_SIGNED)
+    debug.set_trigger_threshold(50)
+    debug.start_measurement(is_eval=True)
+
+    while(not debug.is_measurement_done()):
+        pass
+
+    samples = debug.get_samples()
+
+print("\nDone.")
