71623c032035f98d1b3036032702d6d626ea4e14
Lenard Heß <Lenard.Hess@analog.com>
2022-08-15 10:23:00 +0200

    setup.py: Added explicit encoding selection for reading README.md

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 import setuptools
 from pytrinamic.version import __version__
 
-with open("README.md", "r") as fh:
+with open("README.md", "r", encoding="utf8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
