efabaf58de2aef1be717fb58b3f2ced33e16d689
Sundas <Aysha.Sundas@analog.com>
2022-12-13 14:49:02 +0100

    made cosmetic changes to StallGuard2_demo file

--- examples/modules/TMCM6214/TMCL/StallGuard2_demo.py
+++ examples/modules/TMCM6214/TMCL/StallGuard2_demo.py
@@ -1,70 +1,80 @@
+"""
+Sets the StallGuard2 threshold such that the stall guard value (i.e SG value) is zero
+when the motor comes close to stall and also sets the stop on stall velocity to a value
+one less than the actual velocity of the motor
+"""
 import pytrinamic
 from pytrinamic.connections import ConnectionManager
 from pytrinamic.modules import TMCM6214
 import time
-import numpy as np
 
-def stallguard2_init(init_velocity):
+def stallguard2_init(motor, init_velocity):
+    # Resetting SG2 threshold and stop on stall velocity to zero
     motor.stallguard2.set_threshold(0)
     motor.stallguard2.stop_velocity = 0
+    print("Initial StallGuard2 values:")
+    print(motor.stallguard2)
     print("Rotating...")
     motor.rotate(init_velocity)
     sgthresh = 0
     sgt = 0
-    array = []
+    load_samples = []
     while (sgt == 0) and (sgthresh < 64):
-        array = []
+        load_samples = []
         motor.stallguard2.set_threshold(sgthresh)
         time.sleep(0.2)
         sgthresh += 1
         for i in range(50):
-            array.append(motor.stallguard2.get_load_value())
-        print(sgthresh)
-        print(array)
-        if not np.any(array):
+            load_samples.append(motor.stallguard2.get_load_value())
+        if not any(load_samples):
             sgt = 0
         else:
-            sgt = np.max(array)
-    motor.stallguard2.set_threshold(sgthresh - 1)
+            sgt = max(load_samples)
     while 1:
-        array = []
+        load_samples = []
         for i in range(50):
-            array.append(motor.stallguard2.get_load_value())
-        print(array)
-        if 0 in array or np.max(array) > 450:
+            load_samples.append(motor.stallguard2.get_load_value())
+        print(load_samples)
+        if 0 in load_samples:
             motor.drive_settings.max_current = motor.drive_settings.max_current - 8
         else:
             break
 
     motor.stallguard2.stop_velocity = motor.get_actual_velocity() - 1
-
-
-pytrinamic.show_info()
-
-connectionManager = ConnectionManager()
-with connectionManager.connect() as myInterface:
-    module = TMCM6214(myInterface)
-    motor = module.motors[0]
-
-
-    print("Preparing parameters")
-    # preparing drive settings
-    motor.drive_settings.max_current = 128
-    motor.drive_settings.standby_current = 8
-    motor.drive_settings.boost_current = 0
-    motor.drive_settings.microstep_resolution = motor.ENUM.MicrostepResolution256Microsteps
-    print(motor.drive_settings)
-
-    print(motor.linear_ramp)
-
-    time.sleep(1.0)
-
-    # clear position counter
-    motor.actual_position = 0
-
-    # set up StallGuard2
-    print("Initial StallGuard2 values:")
+    print("Configured StallGuard2 parameters:")
     print(motor.stallguard2)
-    stallguard2_init(10000)
+
+def main():
+    pytrinamic.show_info()
+
+    connection_manager = ConnectionManager()
+    with connection_manager.connect() as my_interface:
+        module = TMCM6214(my_interface)
+        motor = module.motors[0]
+
+        print("Preparing parameters")
+        # preparing drive settings
+        motor.drive_settings.max_current = 128
+        motor.drive_settings.standby_current = 8
+        motor.drive_settings.boost_current = 0
+        motor.drive_settings.microstep_resolution = motor.ENUM.MicrostepResolution256Microsteps
+        print(motor.drive_settings)
+        print(motor.linear_ramp)
+
+        time.sleep(1.0)
+
+        # clear position counter
+        motor.actual_position = 0
+
+        # set up StallGuard2
+        print("Configuring StallGuard2 parameters...")
+        stallguard2_init(motor, init_velocity = 10000)
+        print("Apply load and try to stall the motor...")
+        while not (motor.actual_velocity == 0):
+            pass
+        print("Motor stopped by StallGuard2!")
+
+if __name__ == "__main__":
+        main()
 
 
