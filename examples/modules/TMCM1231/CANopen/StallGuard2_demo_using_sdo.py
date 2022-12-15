f2893e37b91c5451b90e8bbc8c7afc761cc6e82c
Sundas <Aysha.Sundas@analog.com>
2022-12-15 18:43:38 +0100

    added new examples for TMCM1231 using CANopen

---
+++ examples/modules/TMCM1231/CANopen/StallGuard2_demo_using_sdo.py
@@ -0,0 +1,71 @@
+"""
+Sets the StallGuard2 threshold such that the stall guard value (i.e SG value) is zero
+when the motor comes close to stall and also sets the stop on stall velocity to a value
+one less than the actual velocity of the motor
+"""
+import time
+import canopen
+from pytrinamic.modules.CANopen_node import TmcmNode
+
+def stallguard2_init(init_velocity):
+    # Resetting SG2 threshold and stop on stall velocity to zero
+    tmcm_1231.sdo['Stall Guard2 Threshold 1'].raw = 0
+    tmcm_1231.sdo['Stop On Stall 1'].raw = 0
+    print("Rotating...")
+    tmcm_1231.sdo['Target Velocity 1'].raw = init_velocity
+    sgthresh = 0
+    sgt = 0
+    load_samples = []
+    while (sgt == 0) and (sgthresh < 64):
+        load_samples = []
+        tmcm_1231.sdo['Stall Guard2 Threshold 1'].raw = sgthresh
+        time.sleep(0.2)
+        sgthresh += 1
+        for i in range(50):
+            load_samples.append(tmcm_1231.sdo['Actual Load Value 1'].raw )
+        if not any(load_samples):
+            sgt = 0
+        else:
+            sgt = max(load_samples)
+    while 1:
+        load_samples = []
+        for i in range(50):
+            load_samples.append(tmcm_1231.sdo['Actual Load Value 1'].raw )
+        if 0 in load_samples:
+            tmcm_1231.sdo['Absolute Max Current 1'].raw = tmcm_1231.sdo['Absolute Max Current 1'].raw - 1
+        else:
+            break
+
+    tmcm_1231.sdo['Stop On Stall 1'].raw = tmcm_1231.sdo['Velocity Actual Value 1'].raw - 1
+
+def main():
+    with canopen.Network() as network:
+        network.connect(channel='PCAN_USBBUS1', bustype='pcan', bitrate=1_000_000)
+
+        global tmcm_1231
+        tmcm_1231 = TmcmNode(1, 'TMCM-1231.eds')
+        network.add_node(tmcm_1231)
+        tmcm_1231.sdo['Modes of Operation 1'].raw = tmcm_1231.ModeOfOperation.PROFILE_VELOCITY_MODE
+
+        tmcm_1231.load_configuration()
+        tmcm_1231.nmt.state = 'OPERATIONAL'
+
+        tmcm_1231.go_to_operation_enabled()
+        tmcm_1231.sdo['Absolute Max Current 1'].raw = 20
+        tmcm_1231.sdo['Standby Current 1'].raw = 8
+        tmcm_1231.sdo['Microstep Resolution 1'].raw = 8
+
+        # set up StallGuard2
+        print("Configuring StallGuard2 parameters...")
+        stallguard2_init(init_velocity = 10000)
+        print("Apply load and try to stall the motor...")
+        mask = 16384
+        while tmcm_1231.sdo['Statusword 1'].raw & mask != 0:
+            pass
+        print("Motor stopped by StallGuard2!")
+
+        tmcm_1231.nmt.state = 'PRE-OPERATIONAL'
+        tmcm_1231.shutdown()
+
+if __name__ == "__main__":
+        main()
---
+++ examples/modules/TMCM1231/CANopen/rotate_demo_using_pdo.py
@@ -0,0 +1,80 @@
+"""Run a TMCM-1231 in "Profile Velocity Mode" using PDOs.
+
+Includes the recording and plotting of actual velocity.
+
+Tested with:
+* Firmware version 3.26
+* Using the Python canopen package version 2.0.0
+"""
+import time
+import dataclasses
+import canopen
+import matplotlib.pyplot as plt
+from pytrinamic.modules.CANopen_node import TmcmNode
+
+
+samples = []
+
+
+@dataclasses.dataclass
+class Sample:
+    timestamp: float
+    velocity: int
+
+
+def tpdo4_callback(message):
+    global samples
+    velocity = message['Velocity Actual Value 1'].raw
+    timestamp = message.timestamp
+    samples.append(Sample(timestamp=timestamp, velocity=velocity))
+
+
+with canopen.Network() as network:
+    network.connect(channel='PCAN_USBBUS1', bustype='pcan', bitrate=1_000_000)
+
+    tmcm_1231 = TmcmNode(1, 'TMCM-1231.eds')
+    network.add_node(tmcm_1231)
+
+    # Reconfiguration
+    tmcm_1231.sdo[0x1403][1].raw = 0x8000_0381  # disable the RPDO4
+    tmcm_1231.sdo[0x1603][0].raw = 0            # write the number of entries into subindex 0
+    tmcm_1231.sdo[0x1603][1].raw = 0x60400010
+    tmcm_1231.sdo[0x1603][2].raw = 0x60FF0020
+    tmcm_1231.sdo[0x1603][3].raw = 0x60600008
+    tmcm_1231.sdo[0x1603][0].raw = 3            # write the number of entries into subindex 0
+    tmcm_1231.sdo[0x1403][1].raw = 0x4000_0381  # enable the RPDO4
+
+    tmcm_1231.load_configuration()
+    tmcm_1231.nmt.state = 'OPERATIONAL'
+
+    tmcm_1231.go_to_operation_enabled()
+
+    tmcm_1231.tpdo[4].add_callback(tpdo4_callback)
+
+    tmcm_1231.sdo['Absolute Max Current 1'].raw = 50
+    tmcm_1231.sdo['Stop On Stall 1'].raw = 0
+
+    target_velocity = 10000
+    # Profile Velocity Mode
+    tmcm_1231.rpdo[4]['Modes of Operation 1'].raw = tmcm_1231.ModeOfOperation.PROFILE_VELOCITY_MODE
+    tmcm_1231.rpdo[4]['Controlword 1'].raw = 15
+    tmcm_1231.rpdo[4]['Target Velocity 1'].raw = target_velocity
+    tmcm_1231.rpdo[4].transmit()
+    print("Motor rotating...")
+    time.sleep(5)
+    tmcm_1231.rpdo[4]['Target Velocity 1'].raw = 0
+    tmcm_1231.rpdo[4].transmit()
+    print("Motor stopped!")
+    time.sleep(2)
+    tmcm_1231.nmt.state = 'PRE-OPERATIONAL'
+    tmcm_1231.shutdown()
+
+fig, ax = plt.subplots()
+t = [s.timestamp for s in samples]
+v = [s.velocity for s in samples]
+ax.plot(t, v)
+ax.set_xlabel('Time')
+ax.set_ylabel('Velocity')
+plt.show()
+
+
