7bd1b2b89e860bfe111266d7cd5e71cd6b187756
Kahlbaum <Olav.Kahlbaum@analog.com>
2023-07-17 11:21:09 +0200

    Test file for binary TMCL program download included.

---
+++ examples/modules/TMCM1021/TMCL/CoolStep_demo.py
@@ -0,0 +1,74 @@
+import pytrinamic
+from pytrinamic.connections import ConnectionManager
+from pytrinamic.modules import TMCM1021
+import time
+
+pytrinamic.show_info()
+
+# for serial interface
+my_interface = ConnectionManager("--interface serial_tmcl --port COM8 --data-rate 9600").connect()
+
+print(my_interface)
+
+with my_interface:
+    module = TMCM1021(my_interface)
+    motor = module.motors[0]
+
+    # The configuration is based on our PD28-x-1021-TMCL
+    # If you use a different motor be sure you have the right configuration setup otherwise the script may not working.
+
+    print("Preparing parameters...")
+
+    # preparing drive settings
+    motor.drive_settings.max_current = 128
+    motor.drive_settings.standby_current = 0
+    motor.drive_settings.boost_current = 0
+    motor.drive_settings.microstep_resolution = motor.ENUM.MicrostepResolution256Microsteps
+    print(motor.drive_settings)
+
+    # preparing linear ramp settings
+    motor.linear_ramp.max_acceleration = 51200
+    motor.linear_ramp.max_velocity = 102400
+    print(motor.linear_ramp)
+
+    time.sleep(1.0)
+
+    # clear position counter
+    motor.actual_position = 0
+
+    # start rotating motor for 1.5 sek
+    print("Rotating...")
+    motor.rotate(102400)
+    
+    # set up StahlGuard2
+    print("Initial StallGuard2 values:")
+    print(motor.stallguard2)
+
+    motor.stallguard2.calibrate_zero()
+    print("StallGuard2 after calibration:")
+    print(motor.stallguard2)
+
+    print("Try to stop motor...")
+
+    while not(motor.actual_velocity == 0):
+        print("Actual load value: " + str(motor.stallguard2.get_load_value()))
+        time.sleep(0.2)
+    
+    time.sleep(3)
+    print("Start rotating again")
+    motor.rotate(102400)
+
+    print("Initial CoolStep values:")
+    print(motor.coolstep)
+    motor.coolstep.calibrate()
+
+    print("CoolStep after calibration:")
+    print(motor.coolstep)
+
+    time.sleep(10)
+
+    # stop rotating motor
+    print("Stopping...")
+    motor.stop()
+
+print("\nReady.")
---
+++ examples/modules/TMCM1021/TMCL/rotate_demo.py
@@ -0,0 +1,76 @@
+import pytrinamic
+from pytrinamic.connections import ConnectionManager
+from pytrinamic.modules import TMCM1021
+import time
+
+pytrinamic.show_info()
+
+# for serial interface
+myInterface = ConnectionManager("--interface serial_tmcl --port COM8 --data-rate 9600").connect()
+
+
+print(myInterface)
+
+with myInterface:
+    module = TMCM1021(myInterface)
+    motor = module.motors[0]
+
+    # The configuration is based on our PD28-x-1021-TMCL
+    # If you use a different motor be sure you have the right configuration setup otherwise the script may not working.
+
+    print("Preparing parameters...")
+
+    # preparing drive settings
+    motor.drive_settings.max_current = 1000
+    motor.drive_settings.standby_current = 0
+    motor.drive_settings.boost_current = 0
+    motor.drive_settings.microstep_resolution = motor.ENUM.MicrostepResolution256Microsteps
+    print(motor.drive_settings)
+
+    # preparing linear ramp settings
+    motor.linear_ramp.max_acceleration = 25600
+    motor.linear_ramp.max_velocity = 51200
+    print(motor.linear_ramp)
+
+    time.sleep(1.0)
+
+    # clear position counter
+    motor.actual_position = 0
+
+    # start rotating motor for 5 sek
+    print("Rotating...")
+    motor.rotate(102400)
+    time.sleep(5)
+
+    # stop rotating motor
+    print("Stopping...")
+    motor.stop()
+
+    # read actual position
+    print("ActualPosition = {}".format(motor.actual_position))
+    time.sleep(2)
+
+    print("Doubling moved distance.")
+    motor.move_by(motor.actual_position)
+
+    # wait till position_reached
+    while not motor.get_position_reached():
+        print("target position: " + str(motor.target_position) + " actual position: " + str(motor.actual_position))
+        time.sleep(0.2)
+
+    print("Furthest point reached.")
+    print("ActualPosition = {}".format(motor.actual_position))
+
+    # short delay and move back to start
+    time.sleep(3)
+    print("Moving back to 0...")
+    motor.move_to(0)
+
+    # wait until position 0 is reached
+    while not motor.get_position_reached():
+        print("target position: " + str(motor.target_position) + " actual position: " + str(motor.actual_position))
+        time.sleep(0.2)
+
+    print("Reached position 0.")
+
+print("\nReady.")
