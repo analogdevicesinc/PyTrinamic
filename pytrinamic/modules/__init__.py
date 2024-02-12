af7be0442dd7c7d7c81fa2ec4d5d2f951cdb24df
Kahlbaum <Olav.Kahlbaum@analog.com>
2024-02-12 11:43:56 +0100

    Added TMCM-1311.

--- pytrinamic/modules/__init__.py
+++ pytrinamic/modules/__init__.py
@@ -10,6 +10,7 @@ from .TMCM1240 import TMCM1240
 from .TMCM1260 import TMCM1260
 from .TMCM1270 import TMCM1270
 from .TMCM1276 import TMCM1276
+from .TMCM1311 import TMCM1311
 from .TMCM1370 import TMCM1370
 from .TMCM1617 import TMCM1617
 from .TMCM1630 import TMCM1630
