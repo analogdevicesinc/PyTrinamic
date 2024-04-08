d96c6f629dab01844554b70f82191e1035fa6238
ME <Marvin.Ellebrecht@analog.com>
2024-04-08 16:14:31 +0200

    Revert "TMC5130: code improvement. For better look."
    This reverts commit 011971c1093d0cce172ed08f3d55a209fbdd9188.

--- examples/evalboards/TMC5160/rotate_demo.py
+++ examples/evalboards/TMC5160/rotate_demo.py
@@ -1,85 +1,66 @@
 ################################################################################
-# Copyright © 2024 Analog Devices Inc. All Rights Reserved. This software is
+# Copyright © 2019 TRINAMIC Motion Control GmbH & Co. KG
+# (now owned by Analog Devices Inc.),
+#
+# Copyright © 2023 Analog Devices Inc. All Rights Reserved. This software is
 # proprietary & confidential to Analog Devices, Inc. and its licensors.
 ################################################################################
 
 """
-Move a motor back and forth using velocity and position mode of the TMC5130
+Move a motor back and forth using velocity and position mode of the TMC5160
 
 Line 31, we set a lower run/standby current for the motor. Using NEMA17, this should result in a coil current around 800mA.
 If the motor is stalling due to too low current, set motorCurrent higher.
 If a lower value still is needed, set GLOBAL_SCALER register to 128 to half motor current.
-
-In the end, the script  has  3 plots of position, velocity and acceleration
-The acceleration is calculated. Position and speed are read out. The values are not the actual ones.
-
-In the current configuration, it uses a sixPoint ramp. That improves the control system ability.
-
 """
 import time
 import pytrinamic
-import numpy as np
-import math
-import matplotlib.pyplot as plt
-def speed_step2rotation(x): return x / 53687
-def speed_rotation2step(x): return x * 53687
-
 from pytrinamic.connections import ConnectionManager
-from pytrinamic.evalboards import TMC5130_eval
-
+from pytrinamic.evalboards import TMC5160_eval
 
 pytrinamic.show_info()
 
 with ConnectionManager().connect() as my_interface:
     print(my_interface)
 
-    # Create TMC5130-EVAL class which communicates over the Landungsbrücke via TMCL
-    eval_board = TMC5130_eval(my_interface)
+    # Create TMC5160-EVAL class which communicates over the Landungsbrücke via TMCL
+    eval_board = TMC5160_eval(my_interface)
     mc = eval_board.ics[0]
     motor = eval_board.motors[0]
 
-    # it can be used with "Trapezoid Mode" (tapez.) : acceleration, constant speed, deacceleration
-    # or the "6 Point Mode" where the acceleration and deceleration, are splitted in two stages
-    # For "Trapezoid Mode" set A1 = D1 = AMAX = DMAX
-    # For symetric "6 Point Mode" set V >>A1 = D1 > Amax = DMAX -> this is the mode right now
-
-    print("Preparing parameters...")                         #Name     |  Mode   |           Task
-    eval_board.write_register(mc.REG.A1, 1000)         #A1       | 6 piont | initial acceleration between VSTART and V1
-    eval_board.write_register(mc.REG.AMAX, 1000)       #AMAX     | trapez. | accelaration in the end
-    eval_board.write_register(mc.REG.V1, 100000)       #V1       |         | threshold for the  first acceleration phase
-    eval_board.write_register(mc.REG.D1, 1000)         #D1       | 6 piont | de/acceleration in the  end / start
-    eval_board.write_register(mc.REG.DMAX, 1000)       #DMAX     | trapez. | initial deacceleration
-    eval_board.write_register(mc.REG.VSTART, 0)        #VSTART   |         | Motor start velocity
-    eval_board.write_register(mc.REG.VSTOP, 10)        #VSTOP    |         | Motor stop velocity threshold
-    v_max = 7 * 25600  #= 179'200                                                | max velocity
+    print("Preparing parameters...")
+    eval_board.write_register(mc.REG.A1, 1000)
+    eval_board.write_register(mc.REG.V1, 50000)
+    eval_board.write_register(mc.REG.D1, 500)
+    eval_board.write_register(mc.REG.DMAX, 500)
+    eval_board.write_register(mc.REG.VSTART, 0)
+    eval_board.write_register(mc.REG.VSTOP, 10)
+    eval_board.write_register(mc.REG.AMAX, 1000)
 
     # Set lower run/standby current
     motorCurrent = 2
-    motor.set_axis_parameter(motor.AP.RunCurrent, motorCurrent)
+    motor.set_axis_parameter(motor.AP.MaxCurrent, motorCurrent)
     motor.set_axis_parameter(motor.AP.StandbyCurrent, motorCurrent)
 
     # Clear actual positions
     motor.actual_position = 0
 
     print("Rotating...")
-    motor.rotate(-7 * 25600)
+    motor.rotate(7 * 25600)
     time.sleep(5)
-
 
     print("Stopping...")
     motor.stop()
-    time.sleep(2)
+    time.sleep(1)
 
     print("Moving back to 0...")
-    motor.move_to(0, -v_max)
+    motor.move_to(0, 7 * 25600)
 
     # Wait until position 0 is reached
-    T_s = 0.2                   #Sampling Time [seconds]  = Resolution of the plot ; Rage (0.5 ; 0.005)
-
     while motor.actual_position != 0:
-        print(f"Actual position: {motor.actual_position} \t Actual speed: {motor.actual_velocity}")
-        time.sleep(T_s)
+        print("Actual position: " + str(motor.actual_position))
+        time.sleep(0.2)
 
-    print("Reached position 0\t Reached speed 0")
+    print("Reached position 0")
 
 print("\nReady.")
