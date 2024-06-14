4e991e098444af902bb93de8a1b44dcadacff5c1
bp <benjamin.partzsch@analog.com>
2024-06-14 11:59:28 +0200

    Examples: Refactor FirmwareUpdate

--- examples/tools/FirmwareUpdate.py
+++ examples/tools/FirmwareUpdate.py
@@ -14,9 +14,11 @@ import re
 import struct
 import logging
 import intelhex
+import serial
 
 import pytrinamic
 from pytrinamic.connections.connection_manager import ConnectionManager
+from pytrinamic.connections.usb_tmcl_interface import UsbTmclInterface
 from pytrinamic.tmcl import TMCLCommand
 
 # Timeout in seconds for reconnecting to the module after sending the TMCL_BOOT
@@ -57,9 +59,16 @@ def main(cmd_line_args=None):
 
     logging.basicConfig(stream=sys.stdout, level=log_level)
 
+    print("Connecting")
+
+    return firmware_update(connection_manager.connect(), args.hex_file)
+
+
+def firmware_update(iface, hex_file):
+
     ############################### Hex file parsing ###############################
-    print("Opening hex file (" + args.hex_file + ")")
-    file = intelhex.IntelHex(args.hex_file)
+    print("Opening hex file (" + hex_file + ")")
+    file = intelhex.IntelHex(hex_file)
     file.padding = 0x00
     # ########################## Binary data preparation ############################
 
@@ -81,36 +90,35 @@ def main(cmd_line_args=None):
 
     # ############################# Bootloader entry ################################
     # Connect to the evaluation board
-    print("Connecting")
-    my_interface = connection_manager.connect()
 
     # If not already in bootloader, enter it
-    if not "B" in my_interface.get_version_string().upper():
+    if not "B" in iface.get_version_string().upper():
         # Send the boot command
         print("Switching to bootloader mode")
-        my_interface.send_boot(1)
-        my_interface.close()
+        iface.send_boot(1)
 
-        # Reconnect after a small delay
-        print("Reconnecting")
-        timestamp = time.time()
-        while (time.time() - timestamp) < SERIAL_BOOT_TIMEOUT:
-            try:
-                # Attempt to connect
-                my_interface = connection_manager.connect()
-                # If no exception occurred, exit the retry loop
-                break
-            except (ConnectionError, TypeError):
-                my_interface = None
-
-        if not my_interface:
-            print("Error: Timeout when attempting to reconnect to bootloader")
-            exit(1)
+        if isinstance(iface, UsbTmclInterface):
+            iface.close()
+            time.sleep(1)
+            # Reconnect after a small delay
+            print("Reconnecting")
+            timestamp = time.time()
+            while (time.time() - timestamp) < SERIAL_BOOT_TIMEOUT:
+                try:
+                    # Attempt to connect
+                    iface._serial.open()
+                    # If no exception occurred, exit the retry loop
+                    break
+                except serial.serialutil.SerialException:
+                    pass
+            else:
+                print("Error: Timeout when attempting to reconnect to bootloader")
+                return 1
 
     time.sleep(1)
 
     # Retrieve the bootloader version
-    bootloaderVersion = my_interface.get_version_string(1)
+    bootloaderVersion = iface.get_version_string(1)
     found = re.search("\d\d\d\dB\d\d\d", bootloaderVersion)
     if found:
         pattern = found.group(0)[0:4] + "V\d\d\d"
@@ -122,7 +130,7 @@ def main(cmd_line_args=None):
             logging.info(f"Scanning new firmware data for correct module string ({found.group(0)[0:3]}V#.##)")
         else:
             logging.error(f"GetVersion returned invalid answer ({bootloaderVersion})")
-            exit(1)
+            return 1
 
     # Scan for the module string
     found = None
@@ -138,7 +146,7 @@ def main(cmd_line_args=None):
             break
     else:
         print("Error: No matching version string found in firmware image")
-        exit(1)
+        return 1
 
     print("Bootloader version: " + bootloaderVersion)
     print("Firmware version:   " + found.group(0))
@@ -146,11 +154,11 @@ def main(cmd_line_args=None):
     print()
 
     # Get the memory parameters
-    reply = my_interface.send(TMCLCommand.BOOT_GET_INFO, 0, 0, 0)
+    reply = iface.send(TMCLCommand.BOOT_GET_INFO, 0, 0, 0)
     mem_page_size = reply.value
-    reply = my_interface.send(TMCLCommand.BOOT_GET_INFO, 1, 0, 0)
+    reply = iface.send(TMCLCommand.BOOT_GET_INFO, 1, 0, 0)
     mem_start_address = reply.value
-    reply = my_interface.send(TMCLCommand.BOOT_GET_INFO, 2, 0, 0)
+    reply = iface.send(TMCLCommand.BOOT_GET_INFO, 2, 0, 0)
     mem_size = reply.value
 
     logging.debug(f"Bootloader memory page size:      0x{mem_page_size:08X}")
@@ -161,18 +169,18 @@ def main(cmd_line_args=None):
     if not(((mem_page_size & (mem_page_size - 1)) == 0) and mem_page_size != 0):
         print("Error: Page size of module is not a power of two")
         print("Reported page size: {0:X}".format(mem_page_size))
-        exit(1)
+        return 1
 
     # Check if the start addresses match
     if start_address != mem_start_address:
         print("Error: Start address of firmware (0x{0:08X}) does not match start address of bootloader (0x{1:08X})".format(start_address, mem_start_address))
-        exit(1)
+        return 1
 
     # ############################## Firmware upload ################################
 
     # Erase the old firmware
     print("Erasing the old firmware")
-    reply = my_interface.send(TMCLCommand.BOOT_ERASE_ALL, 0, 0, 0)
+    reply = iface.send(TMCLCommand.BOOT_ERASE_ALL, 0, 0, 0)
 
     global current_page
     global current_page_dirty
@@ -188,7 +196,7 @@ def main(cmd_line_args=None):
             raise ValueError
 
         print("Writing page 0x{0:08X}".format(page))
-        my_interface.send(TMCLCommand.BOOT_WRITE_PAGE, 0, 0, current_page)
+        iface.send(TMCLCommand.BOOT_WRITE_PAGE, 0, 0, current_page)
 
 
     # Helper function: Write a 32 bit data block
@@ -206,7 +214,7 @@ def main(cmd_line_args=None):
             current_page_dirty = False
 
         # print("Writing {0:08X} to offset {1:04X} on page {2:08X}".format(writeData, offset, page))
-        my_interface.send(TMCLCommand.BOOT_WRITE_BUFFER, math.floor(offset / 4) % 256, math.floor(math.floor(offset / 4) / 256), write_data)
+        iface.send(TMCLCommand.BOOT_WRITE_BUFFER, math.floor(offset / 4) % 256, math.floor(math.floor(offset / 4) / 256), write_data)
         current_page_dirty = True
 
     print("Uploading new firmware...")
@@ -221,21 +229,21 @@ def main(cmd_line_args=None):
     print()
 
     # Checksum verification
-    reply = my_interface.send(TMCLCommand.BOOT_GET_CHECKSUM, 0, 0, end_address - 1)
+    reply = iface.send(TMCLCommand.BOOT_GET_CHECKSUM, 0, 0, end_address - 1)
     if reply.value != checksum:
         print("Error: Checksums don't match! (Checksum: 0x{0:08X}, received: 0x{1:08X}".format(checksum, reply.value))
-        exit(1)
+        return 1
 
     print("Checksum of the uploaded firmware matches")
     print("Finalizing upload (Writing length and checksum)")
     # Write firmware length
-    my_interface.send(TMCLCommand.BOOT_WRITE_LENGTH, 0, 0, length)
+    iface.send(TMCLCommand.BOOT_WRITE_LENGTH, 0, 0, length)
     # Write firmware checksum
-    my_interface.send(TMCLCommand.BOOT_WRITE_LENGTH, 1, 0, checksum)
+    iface.send(TMCLCommand.BOOT_WRITE_LENGTH, 1, 0, checksum)
 
     # Restart the firmware
     print("Starting the firmware")
-    my_interface.send_start_app()
+    iface.send_start_app()
 
 if __name__ == "__main__":
-    main()
+    sys.exit(main())
