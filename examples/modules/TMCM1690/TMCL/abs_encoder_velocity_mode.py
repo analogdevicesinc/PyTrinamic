fd3f21ee9de3745ce0152ffb24cc58d731393479
bp <benjamin.partzsch@analog.com>
2024-07-17 08:46:21 +0200

    Examples: Clean-up examples for TMCM-1690-TMCL

--- examples/modules/TMCM1690/TMCL/abs_encoder_velocity_mode.py
+++ examples/modules/TMCM1690/TMCL/abs_encoder_velocity_mode.py
@@ -1,13 +1,12 @@
 ################################################################################
-# Copyright © 2021 TRINAMIC Motion Control GmbH & Co. KG
-# (now owned by Analog Devices Inc.),
-#
-# Copyright © 2023 Analog Devices Inc. All Rights Reserved. This software is
+# Copyright © 2024 Analog Devices Inc. All Rights Reserved. This software is
 # proprietary & confidential to Analog Devices, Inc. and its licensors.
 ################################################################################
+"""Example script that shows how to use a TMCM-1690 with an absolute encoder in velocity mode.
 
-if __name__ == '__main__':
-    pass
+The configuration is based on our standard BLDC motor (QBL4208-61-04-013-1024-AT).
+If you use a different motor be sure you have the right configuration setup otherwise the script may not work.
+"""
 
 import pytrinamic
 from pytrinamic.connections import ConnectionManager
@@ -16,83 +15,76 @@ import time
 
 pytrinamic.show_info()
 
-" please select your CAN adapter "
-# myInterface = ConnectionManager("--interface pcan_tmcl").connect()
-myInterface = ConnectionManager("--interface kvaser_tmcl").connect()
-# myInterface = ConnectionManager("--interface serial_tmcl").connect()
+#cm = ConnectionManager("--interface pcan_tmcl")
+cm = ConnectionManager("--interface kvaser_tmcl")
+#cm = ConnectionManager("--interface serial_tmcl")
 
-module = TMCM1690(myInterface)
-print(module)
-motor = module.motors[0]
+with cm.connect() as my_interface:
 
-"""
-    Define motor configuration for the TMCM-1690.
+    module = TMCM1690(my_interface)
+    print(module)
+    motor = module.motors[0]
 
-    The configuration is based on our standard BLDC motor (QBL4208-61-04-013-1024-AT).
-    If you use a different motor be sure you have the right configuration setup otherwise the script may not work.
-"""
+    # motor configuration
+    motor.drive_settings.motor_type = motor.ENUM.MOTOR_TYPE_THREE_PHASE_BLDC
+    motor.drive_settings.pole_pairs = 4
+    motor.drive_settings.max_current = 2000
+    print(motor.drive_settings)
 
-" motor configuration "
-motor.drive_settings.set_motor_type(motor.ENUM.MOTOR_TYPE_THREE_PHASE_BLDC)
-motor.drive_settings.set_pole_pairs(4)
-motor.drive_settings.set_max_current(2000)
-print(motor.drive_settings)
+    # absolute encoder configuration
+    motor.abs_encoder.type = motor.ENUM.ABS_ENCODER_AM4096
+    motor.abs_encoder.type = 1
+    motor.abs_encoder.init_mode = motor.ENUM.ABS_ENCODER_INIT_MODE_USE_OFFSET
 
-" abs encoder sensor configuration "
-motor.abs_encoder.set_type(motor.ENUM.ABS_ENCODER_AM4096)
-motor.abs_encoder.set_direction(1)
-motor.abs_encoder.set_init_mode(motor.ENUM.ABS_ENCODER_INIT_MODE_USE_OFFSET)
+    # motion settings
+    motor.ramp_settings.max_velocity = 2000
+    motor.ramp_settings.max_acceleration = 4000
+    motor.ramp_settings.enabled = 1
+    motor.drive_settings.target_reached_distance = 50
+    motor.drive_settings.target_reached_velocity = 500
+    print(motor.ramp_settings)
 
-" motion settings "
-motor.ramp_settings.set_max_velocity(2000)
-motor.ramp_settings.set_max_acceleration(4000)
-motor.ramp_settings.set_ramp_enabled(1)
-motor.drive_settings.set_target_reached_distance(50)
-motor.drive_settings.set_target_reached_velocity(500)
-print(motor.ramp_settings)
+    # PI configuration
+    motor.pid.torque_p = 300
+    motor.pid.torque_i = 600
+    motor.pid.velocity_p = 300
+    motor.pid.velocity_i = 600
+    motor.pid.position_p = 10
 
-" PI configuration "
-motor.pid.set_torque_p_parameter(300)
-motor.pid.set_torque_i_parameter(600)
-motor.pid.set_velocity_p_parameter(300)
-motor.pid.set_velocity_i_parameter(600)
-motor.pid.set_position_p_parameter(10)
+    # Clear flags
+    motor.set_axis_parameter(motor.AP.IITClearIITExceedFlags, 0)
+    motor.set_axis_parameter(motor.AP.ClearVelocityWindowFlag, 0)
+    motor.set_axis_parameter(motor.AP.ClearPositionWindowFlag, 0)
 
-" Clear flags "
-motor.set_axis_parameter(motor.AP.IITClearIITExceedFlags, 0)
-motor.set_axis_parameter(motor.AP.ClearVelocityWindowFlag, 0)
-motor.set_axis_parameter(motor.AP.ClearPositionWindowFlag, 0)
+    # set commutation mode to absolute encoder
+    motor.drive_settings.commutation_mode = motor.ENUM.COMM_MODE_FOC_ABS_ENCODER
 
-" set commutation mode to absolute encoder "
-motor.drive_settings.set_commutation_mode(motor.ENUM.COMM_MODE_FOC_ABS_ENCODER)
+    # using sine ramp
+    motor.ramp_settings.ramp_type = 1
 
-" using sine ramp "
-motor.ramp_settings.set_ramp_type(1)
+    # Start rotating clockwise at 2000 rpm
+    motor.target_velocity = 2000
 
-" Start rotating clockwise at 2000 rpm "
-motor.set_target_velocity(2000)
+    # wait for 5 second
+    for i in range(5):
+        print("target velocity: {} | actual velocity: {}".format(motor.target_velocity, motor.actual_velocity))
+        time.sleep(1)
 
-" wait for 5 seconds"
-for i in range(5):
-    print("target velocity: %d | actual velocity: %d" % (motor.get_target_velocity(), motor.get_actual_velocity()))
-    time.sleep(1)
+    # using linear ramp
+    motor.ramp_settings.ramp_type = 0
 
-" using linear ramp "
-motor.ramp_settings.set_ramp_type(0)
+    # Start rotating counter clockwise at 2000 rpm
+    motor.target_velocity = -2000
 
-" Start rotating counter clockwise at 2000 rpm "
-motor.set_target_velocity(-2000)
+    # wait for 5 second
+    for i in range(5):
+        print("target velocity: {} | actual velocity: {}".format(motor.target_velocity, motor.actual_velocity))
+        time.sleep(1)
 
-" wait for 5 seconds"
-for i in range(5):
-    print("target velocity: %d | actual velocity: %d" % (motor.get_target_velocity(), motor.get_actual_velocity()))
-    time.sleep(1)
+    # using sine ramp
+    motor.ramp_settings.ramp_type = 1
 
-" using sine ramp "
-motor.ramp_settings.set_ramp_type(1)
+    # Stop the motor.
+    motor.target_velocity = 0
 
-" Stop the motor. "
-motor.set_target_velocity(0)
-
-myInterface.close()
-print("\nReady.")
+    print("\nReady.")
--- examples/modules/TMCM1690/TMCL/hall_position_mode.py
+++ examples/modules/TMCM1690/TMCL/hall_position_mode.py
@@ -1,13 +1,7 @@
 ################################################################################
-# Copyright © 2021 TRINAMIC Motion Control GmbH & Co. KG
-# (now owned by Analog Devices Inc.),
-#
-# Copyright © 2023 Analog Devices Inc. All Rights Reserved. This software is
+# Copyright © 2024 Analog Devices Inc. All Rights Reserved. This software is
 # proprietary & confidential to Analog Devices, Inc. and its licensors.
 ################################################################################
-
-if __name__ == '__main__':
-    pass
 
 import pytrinamic
 from pytrinamic.connections import ConnectionManager
@@ -16,86 +10,82 @@ import time
 
 pytrinamic.show_info()
 
-" please select your CAN adapter "
-# myInterface = ConnectionManager("--interface pcan_tmcl").connect()
-myInterface = ConnectionManager("--interface kvaser_tmcl").connect()
-# myInterface = ConnectionManager("--interface serial_tmcl").connect()
+#cm = ConnectionManager("--interface pcan_tmcl")
+cm = ConnectionManager("--interface kvaser_tmcl")
+#cm = ConnectionManager("--interface serial_tmcl")
 
-module = TMCM1690(myInterface)
-print(module)
-motor = module.motors[0]
+with cm.connect() as my_interface:
 
-"""
-    Define motor configuration for the TMCM-1690.
+    module = TMCM1690(my_interface)
+    print(module)
+    motor = module.motors[0]
 
-    The configuration is based on our standard BLDC motor (QBL4208-61-04-013-1024-AT).
-    If you use a different motor be sure you have the right configuration setup otherwise the script may not work.
-"""
+    # motor configuration
+    motor.drive_settings.motor_type = motor.ENUM.MOTOR_TYPE_THREE_PHASE_BLDC
+    motor.drive_settings.pole_pairs = 4
+    motor.drive_settings.max_current = 2000
+    print(motor.drive_settings)
 
-" motor configuration "
-motor.drive_settings.set_motor_type(motor.ENUM.MOTOR_TYPE_THREE_PHASE_BLDC)
-motor.drive_settings.set_pole_pairs(4)
-motor.drive_settings.set_max_current(2000)
-print(motor.drive_settings)
+    # hall sensor configuration
+    motor.digital_hall.direction = 1
+    motor.digital_hall.sector_offset = 1
+    motor.digital_hall.interpolation = 1
+    print(motor.digital_hall)
 
-" hall sensor configuration "
-motor.digital_hall.set_direction(1)
-motor.digital_hall.set_sector_offset(1)
-motor.digital_hall.set_interpolation(1)
-print(motor.digital_hall)
+    # motion settings
+    motor.ramp_settings.max_velocity = 2000
+    motor.ramp_settings.max_acceleration = 2000
+    motor.ramp_settings.enabled = 1
+    motor.drive_settings.target_reached_distance = 10
+    motor.drive_settings.target_reached_velocity = 500
+    print(motor.ramp_settings)
 
-" motion settings "
-motor.ramp_settings.set_max_velocity(2000)
-motor.ramp_settings.set_max_acceleration(2000)
-motor.ramp_settings.set_ramp_enabled(1)
-motor.drive_settings.set_target_reached_distance(10)
-motor.drive_settings.set_target_reached_velocity(500)
-print(motor.ramp_settings)
+    # PI configuration
+    motor.pid.torque_p = 300
+    motor.pid.torque_i = 600
+    motor.pid.velocity_p = 600
+    motor.pid.velocity_i = 300
+    motor.pid.position_p = 20
+    print(motor.pid)
 
-" PI configuration "
-motor.pid.set_torque_p_parameter(300)
-motor.pid.set_torque_i_parameter(600)
-motor.pid.set_velocity_p_parameter(600)
-motor.pid.set_velocity_i_parameter(300)
-motor.pid.set_position_p_parameter(20)
-print(motor.pid)
+    # Clear flags
+    motor.set_axis_parameter(motor.AP.IITClearIITExceedFlags, 0)
+    motor.set_axis_parameter(motor.AP.ClearVelocityWindowFlag, 0)
+    motor.set_axis_parameter(motor.AP.PositionWindow, 16384000)
+    motor.set_axis_parameter(motor.AP.ClearPositionWindowFlag, 0)
 
-" Clear flags and disable brake chopper"
-motor.set_axis_parameter(motor.AP.IITClearIITExceedFlags, 0)
-motor.set_axis_parameter(motor.AP.ClearVelocityWindowFlag, 0)
-motor.set_axis_parameter(motor.AP.PositionWindow, 16384000)
-motor.set_axis_parameter(motor.AP.ClearPositionWindowFlag, 0)
-motor.brake_chopper.set_enable_parameter(0)
+    # Disable brake chopper
+    motor.brake_chopper.enabled = 0
 
-" set commutation mode to FOC based on hall sensor signals "
-motor.drive_settings.set_commutation_mode(motor.ENUM.COMM_MODE_FOC_DIGITAL_HALL)
+    # set commutation mode to absolute encoder
+    motor.drive_settings.commutation_mode = motor.ENUM.COMM_MODE_FOC_DIGITAL_HALL
 
-" clear actual position "
-motor.set_actual_position(0)
+    # set position counter to zero
+    motor.actual_position = 0
 
-" move to zero position"
-motor.set_target_position(0)
+    # move to zero position
+    motor.target_position = 0
 
-" using linear ramp "
-motor.ramp_settings.set_ramp_type(0)
+    # using linear ramp
+    motor.ramp_settings.ramp_type = 0
 
-print("starting positioning")
-motor.set_target_position(4000)
+    print("starting positioning")
+    motor.target_position = 4000
 
-" wait for position reached "
-while not motor.get_position_reached():
-    print("target position: %d | actual position: %d" % (motor.get_target_position(), motor.get_actual_position()))
-    time.sleep(0.2)
+    # wait for position reached
+    while not motor.get_position_reached():
+        print("target position: {} | actual position: {}".format(motor.target_position, motor.actual_position))
+        time.sleep(0.2)
 
-" using sine ramp "
-motor.ramp_settings.set_ramp_type(1)
-" move back to zero"
-motor.set_target_position(0)
+    # using sine ramp
+    motor.ramp_settings.ramp_type = 1
 
-" wait for position reached "
-while not motor.get_position_reached():
-    print("target position: %d | actual position: %d" % (motor.get_target_position(), motor.get_actual_position()))
-    time.sleep(0.2)
+    # move back to zero
+    motor.target_position = 0
 
-myInterface.close()
-print("\nReady.")
+    # wait for position reached
+    while not motor.get_position_reached():
+        print("target position: {} | actual position: {}".format(motor.target_position, motor.actual_position))
+        time.sleep(0.2)
+
+    print("\nReady.")
