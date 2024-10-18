c78e62a06422254be4fcbdb86449c89efb4b25d8
bp <benjamin.partzsch@analog.com>
2024-10-18 17:08:39 +0200

    TMC9660: Make the Eval and the IC a UblApiDevice, driving the UBL read/write functions

--- pytrinamic/evalboards/TMC9660_eval.py
+++ pytrinamic/evalboards/TMC9660_eval.py
@@ -10,15 +10,25 @@ from typing import Union
 
 from pytrinamic.modules import Parameter
 from pytrinamic.ic import TMC9660
+from pytrinamic.ic.tmc_ic import UblApiDevice
+from pytrinamic.tmcl import TMCLCommand
 
 
-class TMC9660_eval:
+class TMC9660_eval(UblApiDevice):
     """Generic class for TMC9660 eval boards."""
     def __init__(self, connection):
         self._connection = connection
         self._ap_index_bit_width = 12
         self._module_id = 1
         self.ics = [TMC9660()]
+
+    # Implementation of UblApiDevice-write_register
+    def write_register(self, register_address, block, value):
+        return self._connection.write_register(register_address, TMCLCommand.WRITE_MC, block, value, self._module_id)
+
+    # Implementation of UblApiDevice-read_register
+    def read_register(self, register_address, block, signed=False):
+        return self._connection.read_register(register_address, TMCLCommand.READ_MC, block, self._module_id, signed)
         
     def get_axis_parameter(self, ap: Union[Parameter, int]):
         return self._connection.get_axis_parameter(ap, 0, index_bit_width=self._ap_index_bit_width)
