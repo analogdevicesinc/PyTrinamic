05fe7c00dc10d2f9c8ff9471b58d137e1970346f
Lenard Heß <Lenard.Hess@analog.com>
2024-11-15 15:16:11 +0100

    TMC9660_eval: Updated connection to use module ID 3
    The TMC9660-*-EVALs will be reachable via module ID 3 when talking to the
    TMC-EvalSystem

--- pytrinamic/evalboards/TMC9660_eval.py
+++ pytrinamic/evalboards/TMC9660_eval.py
@@ -20,33 +20,67 @@ from pytrinamic.tmcl import TMCLCommand
 
 class TMC9660_eval(UblApiDevice):
     """Generic class for TMC9660 eval boards."""
-    def __init__(self, connection):
+    def __init__(self, connection, default_module_id=3):
         self._connection = connection
         self._ap_index_bit_width = 12
-        self._module_id = 1
+        self._module_id = default_module_id
         self.ics = [TMC9660()]
 
     def write_register(self, register_address, block, value):
         """Implementation of the UblApiDevice::write_register() function."""
-        return self._connection.write_register(register_address, TMCLCommand.WRITE_MC, block, value, self._module_id)
+        return self._connection.write_register(
+            register_address,
+            TMCLCommand.WRITE_MC,
+            block,
+            value,
+            module_id=self._module_id
+        )
 
     def read_register(self, register_address, block, signed=False):
         """Implementation of the UblApiDevice::read_register() function."""
-        return self._connection.read_register(register_address, TMCLCommand.READ_MC, block, self._module_id, signed)
+        return self._connection.read_register(
+            register_address,
+            TMCLCommand.READ_MC,
+            block,
+            module_id=self._module_id,
+            signed=signed,
+        )
         
     def get_axis_parameter(self, ap: Parameter):
         signed = True if ap.datatype == Parameter.Datatype.SIGNED else False
-        return self._connection.get_axis_parameter(ap.index, 0, signed, index_bit_width=self._ap_index_bit_width)
+        return self._connection.get_axis_parameter(
+            ap.index,
+            0,
+            signed,
+            module_id=self._module_id,
+            index_bit_width=self._ap_index_bit_width,
+        )
 
     def set_axis_parameter(self, ap: Parameter, value):
-        return self._connection.set_axis_parameter(ap.index, 0, value, index_bit_width=self._ap_index_bit_width)
+        return self._connection.set_axis_parameter(
+            ap.index,
+            0,
+            value,
+            module_id=self._module_id,
+            index_bit_width=self._ap_index_bit_width,
+        )
 
     def get_global_parameter(self, gp: Parameter, bank):
         signed = True if gp.datatype == Parameter.Datatype.SIGNED else False
-        return self._connection.get_global_parameter(gp.index, bank, signed)
+        return self._connection.get_global_parameter(
+            gp.index,
+            bank,
+            signed,
+            module_id=self._module_id,
+        )
 
     def set_global_parameter(self, gp: Parameter, bank, value):
-        return self._connection.set_global_parameter(gp.index, bank, value)
+        return self._connection.set_global_parameter(
+            gp.index,
+            bank,
+            value,
+            module_id=self._module_id,
+        )
     
 
 class TMC9660_3PH_eval(TMC9660_eval):
