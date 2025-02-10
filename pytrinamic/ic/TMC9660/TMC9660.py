7c25dc63fc555be4af545d387a97f6476c476c90
bp <benjamin.partzsch@analog.com>
2025-02-10 15:39:24 +0100

    TMC9660: Fix missing register address bit width specification
    Reading and writing some registers failed because the register address bit width was set to 10 (default) instead of 11.

--- pytrinamic/ic/TMC9660/TMC9660.py
+++ pytrinamic/ic/TMC9660/TMC9660.py
@@ -80,6 +80,7 @@ class TMC9660(TMCIc, RegisterApiDevice, ParameterApiDevice):
         super().__init__("TMC9660", self.__doc__)
 
         self._connection = connection
+        self._connection._default_register_address_bit_width = 11
         self._ap_index_bit_width = 12
         self._module_id = module_id
         self.datalogger = DataLogger(connection, module_id=module_id)
