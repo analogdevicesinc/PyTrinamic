22b06a001bc2821163633497259b1b1738648365
bp <benjamin.partzsch@analog.com>
2025-02-17 10:36:10 +0100

    Examples/TMC9660: Add a new example for the register mode

--- examples/evalboards/TMC9660/reg/with_landungsbruecke/tmc9660_3ph_eval_datalogger_demo.py
+++ examples/evalboards/TMC9660/reg/with_landungsbruecke/tmc9660_3ph_eval_datalogger_demo.py
@@ -15,9 +15,9 @@ running this script.        |LB   |==|TMC9660-3PH-EVAL   |
                             +-----+  +-------------------+
 
 Expected output:
-  DataLogger.Log(rate_hz=25000.0, samples=[-21, 43, -37, -29, -93, -45, -21, -61, 11, 11])
-  DataLogger.Log(rate_hz=25000.0, samples=[51, -5, -21, 67, 51, 19, 59, -21, 27, 51])
-  DataLogger.Log(rate_hz=25000.0, samples=[-9, -49, -65, -57, -81, -105, -25, -73, -105, -9])
+  DataLogger.LogData(samples=[-8, -48], request_object=<pytrinamic.ic.TMC9660.MCCmap._ALL_REGISTERS._ADC_I1_I0_SCALED._I0 object at 0x00000192DFCE1D50>)
+  DataLogger.LogData(samples=[-32, 24], request_object=<pytrinamic.ic.TMC9660.MCCmap._ALL_REGISTERS._ADC_I1_I0_SCALED._I1 object at 0x00000192DFCE1D20>)
+  DataLogger.LogData(samples=[-8, 0], request_object=<pytrinamic.ic.TMC9660.MCCmap._ALL_REGISTERS._ADC_I3_I2_SCALED._I2 object at 0x00000192DFCE1DE0>)
 """
 
 from pytrinamic.connections import ConnectionManager
@@ -39,7 +39,6 @@ with ConnectionManager().connect() as my_interface:
        TMC9660.MCC.ADC_I1_I0_SCALED.I1,
        TMC9660.MCC.ADC_I3_I2_SCALED.I2,
     ]
-    
 
     dl.start_logging()
 
