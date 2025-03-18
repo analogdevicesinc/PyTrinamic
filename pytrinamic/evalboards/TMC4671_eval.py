1a93007b5564e977b1e9b725a9c6569082e0c43a
bp <benjamin.partzsch@analog.com>
2025-03-18 18:03:15 +0100

    TMC4671-EVAL: Remove wrong forwarding of the connection object
    The TMC4671 class can only make use "IC-interface" class objects, not of "TMCL-interface" class objects.

--- pytrinamic/evalboards/TMC4671_eval.py
+++ pytrinamic/evalboards/TMC4671_eval.py
@@ -19,7 +19,7 @@ class TMC4671_eval(TMCLEval):
     def __init__(self, connection, module_id=1):
         TMCLEval.__init__(self, connection, module_id)
         self.motors = [self._MotorTypeA(self, 0)]
-        self.ics = [TMC4671(connection)]
+        self.ics = [TMC4671()]
         self.datalogger = DataLogger(connection, module_id)
 
     # Use the motion controller channel for register access
