2ac53890caf84ae957fa9acb12ef80c0fd90684e
solotopolono07 <nilesh.joshi@analog.com>
2025-04-04 13:27:10 +0100

    tmcm_1240_profile_position_mode_demo

--- examples/modules/TMCM1240/CANopen/tmcm_1240_profile_position_mode_demo.py
+++ examples/modules/TMCM1240/CANopen/tmcm_1240_profile_position_mode_demo.py
@@ -1,3 +1,7 @@
+################################################################################
+# Copyright © 2025 Analog Devices Inc. All Rights Reserved.
+# This software is proprietary to Analog Devices, Inc. and its licensors.
+################################################################################
 import time
 import canopen
 from pytrinamic.modules.canopen_node import TmcmNode
@@ -11,22 +15,13 @@ with canopen.Network() as network:
     network.add_node(tmcm_1240)
 
     # LOADING CONFIGURATION FOR CANOPEN NODE FILE 
-
-
-  
     tmcm_1240.load_configuration()
-   
     tmcm_1240.nmt.state = 'OPERATIONAL'
     tmcm_1240.go_to_operation_enabled()
     tmcm_1240.shutdown()
-    
-    
     tmcm_1240.sdo['Absolute Max Current 1'].raw = 80# CURRENT SETTINGS 
     tmcm_1240.sdo['Switch Parameters 1'].raw = 3# DISSABLE LIMIT SWITCHES
-    
     tmcm_1240.sdo['Controlword 1'].raw= 6 # State transition to Shut Down
-
-   
     tmcm_1240.sdo['Controlword 1'].raw= 7 # State transition to Switched On
     tmcm_1240.go_to_operation_enabled()
     # Six Point Ramp Parameters    
@@ -43,19 +38,11 @@ with canopen.Network() as network:
     # PROFILE POSTION MODE
     
     tmcm_1240.sdo['Modes of Operation 1'].raw=1
-    
-
-    
     tmcm_1240.sdo['Target Position 1'].raw = 200000 # TARGET VELOCITY 
-    
     tmcm_1240.sdo['Controlword 1'].raw = 31 
     print("Motor starts rotating ") 
     time.sleep(50)
-  
-
     print(tmcm_1240.get_state())
     print("Motor Stoped ") 
-   
-
     tmcm_1240.shutdown() # SWITCH ON DISSABLE 
     print(tmcm_1240.get_state())
